b75e7d1c6f307119cf2dceff042f69aec2625ba5
Fabien Casenave <fabien.casenave@safran.fr>
2016-04-07 14:17:14 +0200

    minor changes in initialization of instance of Numerical/GramSchmidt.py

--- Numerical/GramSchmidt.py
+++ Numerical/GramSchmidt.py
@@ -1,45 +1,39 @@
 import numpy as np
 
-class GramSchmidt():
+class GramSchmidt(object):
     """Modified Gram-Schmidt orthonormalization routine, with user-provided scalar product
     
     """
     
-    def __init__(self):
-        self.sizeVec = None
-        self.nbVec = None
-        self.u = None
-        self.e = None
-
-    def Init(self, v):
+    def __init__(self, A, v):
+        self.A = A
+        self.v = v
         shape = v.shape
         self.nbVec   = shape[0]
         self.sizeVec = shape[1]
         self.u = np.zeros((self.nbVec,self.sizeVec), dtype = float)
         self.e = np.zeros((self.nbVec,self.sizeVec), dtype = float)
-        return
 
-    def ScalProd(self, A, v, u):
-        return np.dot(v, A.dot(u))
+    def ScalProd(self, v, u):
+        return np.dot(v, self.A.dot(u))
 
-    def Proj(self, A, v, u):
-        return (self.ScalProd(A, v, u)/self.ScalProd(A, u, u))*u
+    def Proj(self, v, u):
+        return (self.ScalProd(v, u)/self.ScalProd(u, u))*u
 
-    def Compute(self, A, v):
-        self.Init(v)
+    def Compute(self):
         for i in xrange(self.nbVec):
-            self.u[i] = v[i]
+            self.u[i] = self.v[i]
             for j in xrange(i):
-                self.u[i] = self.u[i] - self.Proj(A, self.u[i], self.u[j])
-            self.e[i] = self.u[i]/np.sqrt(self.ScalProd(A, self.u[i], self.u[i]))
+                self.u[i] = self.u[i] - self.Proj(self.u[i], self.u[j])
+            self.e[i] = self.u[i]/np.sqrt(self.ScalProd(self.u[i], self.u[i]))
         return
 
-    def Check(self, A):
+    def Check(self):
         import OTTools.Helpers.TextFormatHelper as TFH
         matCheck = np.zeros((self.nbVec,self.nbVec), dtype = float)
         for i in xrange(self.nbVec):
             for j in xrange(self.nbVec):
-                matCheck[i,j] = self.ScalProd(A, self.e[i], self.e[j])
+                matCheck[i,j] = self.ScalProd(self.e[i], self.e[j])
         matCheck -= np.eye(self.nbVec)
         
         print("Gram-Schmidt relative error = "+ TFH.TFormat.GoodBad(np.linalg.norm(matCheck)/np.linalg.norm(np.eye(self.nbVec)),1.e-10))
@@ -47,11 +41,11 @@ class GramSchmidt():
         
 
 def CheckIntegrity():
-    vec = np.array([np.random.rand(10), np.random.rand(10), np.random.rand(10)])
-    A = np.eye(10)
-    GS = GramSchmidt()
-    GS.Compute(A,vec)
-    GS.Check(A)
+    v  = np.random.rand(3,10)
+    A  = np.eye(10)
+    GS = GramSchmidt(A, v)
+    GS.Compute()
+    GS.Check()
     return 'ok'
         
         
