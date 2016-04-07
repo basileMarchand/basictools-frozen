797156a2fd652b34a4d5121b916d5671b5eff984
Fabien Casenave <fabien.casenave@safran.fr>
2016-04-07 14:52:05 +0200

    move Zebulon relative function of ../IO/CodeInterface.py to rmtools repository

--- IO/CodeInterface.py
+++ IO/CodeInterface.py
@@ -62,29 +62,7 @@ class Interface(object):
         with open(filenameDir, 'r') as File:
             string = File.read()
         
-        return string
-      
-    def WriteCubeZebulonMesher(self, mu1, mu2, mu3, nCuts):
-        with open(self.processDirectory + 'cube.mast', 'w') as mast_file:
-            mast_file.write("****master\n***geometry\n**point point0\n*position 0. 0. 0.\n**point point1\n*position "+str(mu1)+" 0. 0.")
-            mast_file.write("\n**point point2\n*position  "+str(mu1)+" "+str(mu2)+" 0.\n**point point3\n*position 0. "+str(mu2)+" 0.\n")
-            mast_file.write("**line line0\n*p1 point0\n*p2 point1\n*cuts "+str(nCuts)+"\n**line line1\n*p1 point1\n*p2 point2\n*cuts "+str(nCuts)+"\n")
-            mast_file.write("**line line2\n*p1 point2\n*p2 point3\n*cuts "+str(nCuts)+"\n**line line3\n*p1 point3\n*p2 point0\n*cuts "+str(nCuts)+"\n")
-            mast_file.write("**domain domain1 Ruled0\n*name R\n*element c2d4\n*method 1\n*b1 line0\n*b2 line1\n*b3 line2\n*b4 line3\n")
-            mast_file.write("***mesher\n**extension\n*elset ALL_ELEMENT\n*prog 1.00000\n*fusion 0.00100000\n*distance "+str(mu3)+"\n*num "+str(nCuts)+"\n")
-            mast_file.write("*dir ( 0.00000 0.00000 1.00000 )\n***plotting\n**default_options 1 1 1\n****return")
-        mast_file.close()  
-      
-    def WriteZebulonTablesBC(self):
-        self.parameters['table'] = ""
-        for i in xrange(len(self.ptab['names'])):
-          self.parameters['table']  = self.parameters['table'] + '**name '+self.ptab['names'][i]+'\n'
-          self.parameters['table']  = self.parameters['table'] + '*time  '+str(self.ptab['times'][i][0])+'  '+str(self.ptab['times'][i][1])+'\n'
-          self.parameters['table']  = self.parameters['table'] + '*value  '+str(self.ptab['values'][i][0])+'  '+str(self.ptab['values'][i][1])+'\n'
-     
-        self.parameters['bc'] = "**surface_heat_flux\n"
-        for i in xrange(len(self.bc['surface_heat_flux']['bsets'])):
-          self.parameters['bc']  = self.parameters['bc']+self.bc['surface_heat_flux']['bsets'][i]+'  '+str(self.bc['surface_heat_flux']['values'][i])+'  '+self.bc['surface_heat_flux']['ptabs'][i]+'\n'         
+        return string       
                    
 
 def CheckIntegrity():
@@ -106,17 +84,9 @@ def CheckIntegrity():
     interface.parameters['iteration']     = 1000
     interface.parameters['ratio']         = 0.001
     interface.parameters['algorithm']     = 'p1p2p3'
-
-    interface.ptab['names']               = ['ptab']
-    interface.ptab['times']               = [[0., 10000000.]]
-    interface.ptab['values']              = [[1., 1.]]
-
-    interface.bc['surface_heat_flux']     = {}
-    interface.bc['surface_heat_flux']['bsets']  = ['face_x_0', 'face_x_1']
-    interface.bc['surface_heat_flux']['values'] = [1000., 1000.]
-    interface.bc['surface_heat_flux']['ptabs']  = ['ptab', 'ptab']
-
-    interface.WriteZebulonTablesBC()
+    
+    interface.parameters['bc']            = 'myBC'
+    interface.parameters['table']         = 'myTable'
         
     interface.parameters['conductivity']  = '280.+100.*atan(0.01*(1100-temperature));'
     interface.parameters['coefficient']   = '8.*(430.+40.*atan(0.01*(temperature-500.)))*(1.5-0.5*exp(-200./((temperature-1200.)*(temperature-1200.))));'
@@ -124,7 +94,6 @@ def CheckIntegrity():
     interface.processDirectory  = T.TestTempDir.GetTempPath()
     interface.WriteFile(1)
     #interface.SingleRunComputation(1)
-    interface.WriteCubeZebulonMesher(1., 1., 1., 3)
     return 'ok'
         
         
