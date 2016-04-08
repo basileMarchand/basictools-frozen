1157fc95dfffb9f2e41efd3959177120c594fd6d
Julien Cortial <julien.cortial@safran.fr>
2016-04-08 14:32:52 +0200

    Add elementary POD and gappy POD tools

---
+++ Numerical/GappyPod.py
@@ -0,0 +1,83 @@
+import numpy as np
+import numpy.linalg as la
+import scipy.linalg as sla
+
+
+def deim(basis):
+    """
+    Generate empirical sample entries using the standard DEIM algorithm [1].
+
+    Parameters
+    ----------
+    basis : array-like
+        An orthonormal multivector.
+
+    Returns
+    -------
+    ndarray
+        An 1-d array containing the sample entries.
+
+    Notes
+    -----
+        A multivector is (n, N)-array where n is the number of vectors and N
+        the numbers of dofs.
+
+        [1] S. Chaturantabut & D. Sorensen, "Nonlinear Model Reduction via
+        Discrete Empirical Interpolation", SIAM Journal on Scientific
+        Computing, vol. 32, num. 5, pp. 2737-2764, 2010.
+    """
+    basis_rank = basis.shape[0]
+    sample_entries = np.empty(basis_rank, dtype=np.int_)
+
+    if basis_rank > 0:
+        sample_entries[0] = np.argmax(np.fabs(basis[0, :]))
+
+        sampled_basis = np.empty((basis_rank, basis_rank - 1), dtype=np.double)
+
+    for k in xrange(1, basis_rank):
+        added_basis_slice = sampled_basis[:, k - 1]
+        np.copyto(added_basis_slice, basis[:, sample_entries[k - 1]], casting='no')
+
+        s = slice(0, k)
+        interpolation_matrix = np.transpose(sampled_basis[s, s])
+        rhs = sampled_basis[k, s]
+        components = la.solve(interpolation_matrix, rhs)
+        np.negative(components, out=components)
+        residual = np.dot(components, basis[s, :])
+        residual += basis[k, :]
+
+        sample_entries[k] = np.argmax(np.fabs(residual))
+
+    return sample_entries
+
+
+def q_deim(basis):
+    """
+    Generate empirical sample entries using the Q-DEIM algorithm [1].
+
+    Parameters
+    ----------
+    basis : array-like
+        An orthonormal multivector.
+
+    Returns
+    -------
+    ndarray
+        An 1-d array containing the sample entries.
+
+    Notes
+    -----
+        A multivector is (n, N)-array where n is the number of vectors and N
+        the numbers of dofs.
+
+        [1] Z. Drmac and S. Gugercin. A New Selection Operator for the Discrete
+        Empirical Interpolation Method : Improved a priori error bound and
+        extensions, 2015. URL: http://arxiv.org/abs/1505.00370.
+    """
+    basis_rank = basis.shape[0]
+
+    if basis_rank == 0:
+        return np.empty(basis_rank, dtype=np.int_)
+
+    Q, R, P = sla.qr(basis, pivoting=True)
+    return P[:basis_rank]
---
+++ Numerical/Pod.py
@@ -0,0 +1,115 @@
+import numpy as np
+import numpy.linalg as la
+
+
+def pod_basis(snapshots, rank_max=None, truncation_tol=None):
+    """
+    Generate a POD basis from a set of snapshots
+
+    Parameters
+    ----------
+    snapshots : (n, N) array-like
+        A multivector of n snapshots of size N.
+    rank_max : int, optional
+        An upper bound on the size of the POD basis. Truncation will be
+        performed if necessary.  By default, do not perform truncation.
+    truncation_tol : float, optional
+        An energy-based criterion controlling truncation. Keep as many vectors
+        in the POD basis such that the energy fraction of the discarded
+        components is lower than truncation_tol. By default, do not perform
+        truncation.
+
+    Returns
+    -------
+    (m, N) ndarray
+        An orthonormal multivector representing the POD basis.
+
+    Notes
+    -----
+        A multivector is an (n, N)-array where:
+          n denotes the number of vectors
+          N denotes the number of entries in each vector
+    """
+    result, _ = pod_basis_and_singular_values(snapshots, rank_max, \
+            truncation_tol)
+    return result
+
+
+def pod_basis_and_singular_values(snapshots, rank_max=None, truncation_tol=None):
+    """
+    Generate a POD basis and the associated singular values from a set of
+    snapshots
+
+    Parameters
+    ----------
+    snapshots : (n, N) array-like
+        A multivector of n snapshots of size N.
+    rank_max : int, optional
+        An upper bound on the size of the POD basis. Truncation will be
+        performed if necessary.  By default, do not perform truncation.
+    truncation_tol : float, optional
+        An energy-based criterion controlling truncation. Keep as many vectors
+        in the POD basis such that the energy fraction of the discarded
+        components is lower than truncation_tol. By default, do not perform
+        truncation.
+
+    Returns
+    -------
+    (m, N) ndarray
+        An orthonormal multivector representing the POD basis.
+    (m,) ndarray
+        A 1-d vector of singular values
+
+    Notes
+    -----
+        A multivector is an (n, N)-array where:
+          n denotes the number of vectors
+          N denotes the number of entries in each vector
+    """
+    snapshot_count = snapshots.shape[0]
+
+    if rank_max is None:
+        rank_max = snapshot_count
+
+    v, s, u = la.svd(snapshots, full_matrices=False, compute_uv=True)
+
+    if truncation_tol is not None:
+        truncation_thresholds = discarded_energy_fractions(s)[::-1]
+        drop_count = \
+                min(np.searchsorted(truncation_thresholds, truncation_tol), \
+                snapshot_count - 1)
+        rank_max = min(snapshot_count - drop_count, rank_max)
+
+    return (u, s) if rank_max >= snapshot_count else (u[:rank_max, :], s[:rank_max])
+
+
+def discarded_energy_fractions(singular_values):
+    """
+    Compute the fraction of energy that would be discarded if truncation was
+    performed right after the corresponding singular value.
+
+    Parameters
+    ----------
+    singular_values : (n,) array-like
+        Singular values, sorted in decreasing order
+
+    Returns
+    -------
+    (n,) ndarray
+        Energy fractions
+    """
+    modal_energies_squared = np.square(singular_values)
+
+    reversed_modal_energies_squared = modal_energies_squared[::-1]
+    reversed_cumulated_energies_squared = \
+            np.cumsum(reversed_modal_energies_squared, out=reversed_modal_energies_squared)
+    cumulated_energies_squared = reversed_cumulated_energies_squared[::-1]
+    cumulated_energies = np.sqrt(cumulated_energies_squared, \
+            out=cumulated_energies_squared)
+
+    result = np.empty_like(singular_values)
+    result[-1] = 0.0
+    total_energy = cumulated_energies[0]
+    np.divide(cumulated_energies[1:], total_energy, out=result[:-1])
+
+    return result
