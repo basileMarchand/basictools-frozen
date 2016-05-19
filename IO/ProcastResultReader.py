3a94490cb3203ef904a2e14cf1bc157710540e55
Fabien Casenave <fabien.casenave@safran.fr>
2016-05-19 09:49:43 +0200

    add ProcastResultReader.py to read .unv results from foundry processes simulated by Procast

---
+++ IO/ProcastResultReader.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+import OTTools.FE.UnstructuredMesh  as UM
+import numpy as np
+
+
+
+def ReadResult(nbNodes, fileName=None, string=None):
+    from cStringIO import StringIO
+
+    if fileName is not None:
+      f = open(fileName, 'r')
+      string = f.read()
+      f.close()
+
+    timeSteps   = []
+    temperature = np.empty((nbNodes,0),dtype=np.double)
+
+    string = StringIO(string)
+    
+    for line in string:
+       l = line.strip('\n').lstrip().rstrip()
+       if len(l) == 0: continue
+
+       if l.find("2         4         1         5         2         1")>-1:
+           line = string.readline()
+           l = line.strip('\n').lstrip().rstrip()
+           s = l.split()
+           timeSteps.append(int(s[3]))
+           newTemp = np.empty((nbNodes,1),dtype=np.double)
+
+           line = string.readline(); line = string.readline(); l = line.strip('\n').lstrip().rstrip()
+           while(True):
+               s = l.split()
+               index = int(s[0])-1
+               line = string.readline()
+               l = line.strip('\n').lstrip().rstrip()
+               s = l.split()
+               newTemp[index] = float(s[0])
+               line = string.readline()
+               l = line.strip('\n').lstrip().rstrip()
+               if len(l) == 0: continue     
+               if l.find("-1") > -1:
+                   break
+           temperature = np.hstack((temperature, newTemp))
+           continue
+        
+    return timeSteps, temperature
+
+
+
+def CheckIntegrity():
+
+    __teststring = """
+    -1
+    55
+TEMPERATURE
+ProCAST RESULTS, ESI
+
+
+
+         2         4         1         5         2         1
+         2         1         0         0
+  0.00000E+00
+         1
+  1.53000E+03
+         2
+  1.53000E+03
+         3
+  1.53000E+03
+         4
+  1.53000E+03
+    -1
+    -1
+    55
+TEMPERATURE
+ProCAST RESULTS, ESI
+
+
+
+         2         4         1         5         2         1
+         2         1         0      2140
+  2.12800E+03
+         1
+  5.41073E+02
+         2
+  5.41557E+02
+         3
+  5.42009E+02
+         4
+  5.42137E+02
+    -1
+
+"""
+    nbNodes = 4
+    timeSteps, temperature = ReadResult(nbNodes, string=__teststring)
+    return 'ok'
+    
+if __name__ == '__main__':
+    print(CheckIntegrity())# pragma: no cover   
--- IO/__init__.py
+++ IO/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
-__all__ = ['PickleTools','AcsReader','CodeInterface','GoefReader','GoefWriter','GmshReader','GmshWriter','InpReader','InputFile','PickleTools','StlReader','XdmfReader','XdmfTools','XdmfWriter','ZebulonIO']
+__all__ = ['PickleTools','AcsReader','CodeInterface','GoefReader','GoefWriter','GmshReader','GmshWriter','InpReader','InputFile','PickleTools','StlReader','XdmfReader','XdmfTools','XdmfWriter','ZebulonIO','ProcastResultReader']
 
