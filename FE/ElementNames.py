224eb228ab12dba877ea3e99c9cad87da01ce1bc
Felipe Bordeu <Felipe.Bordeu@safran.fr>
2016-12-27 10:50:37 +0100

    add nodes numbers for all elements types

--- FE/ElementNames.py
+++ FE/ElementNames.py
@@ -1,35 +1,58 @@
 # -*- coding: utf-8 -*-
+numberOfNodes = {}
 
 #0d
 Point_1  = 'point1'
-
+numberOfNodes[Point_1] = 1;
 #1d
 #linear
 Bar_2 = 'bar2'
+numberOfNodes[Bar_2] = 2;
 #quadratic
 Bar_3 = 'bar3'
+numberOfNodes[Bar_3] = 3;
 
 #2d
 #linear
 Triangle_3 = 'tri3'
+numberOfNodes[Triangle_3] = 3;
 Quadrangle_4  = 'quad4'
+numberOfNodes[Quadrangle_4] = 4;
 #quadratic
 Triangle_6 = 'tri6'
+numberOfNodes[Triangle_6] = 6;
 Quadrangle_8  = 'quad8'
+numberOfNodes[Quadrangle_8] = 8;
 Quadrangle_9  = 'quad9'
-
+numberOfNodes[Quadrangle_9] = 9;
 #3d
 #linear
 Tetrahedron_4 = 'tet4'
+numberOfNodes[Tetrahedron_4] = 4;
 Pyramid_5  = 'pyr5'
+numberOfNodes[Pyramid_5] = 5;
 Wedge_6 = 'wed6'
+numberOfNodes[Wedge_6] = 6;
 Hexaedron_8 = 'hex8'
+numberOfNodes[Hexaedron_8] = 8;
+
 #quadratic
 Tetrahedron_10 = 'tet10'
+numberOfNodes[Tetrahedron_10] = 10;
 Pyramid_13  = 'pyr13'
+numberOfNodes[Pyramid_13] = 13;
 Wedge_15 = 'hex15'
+numberOfNodes[Wedge_15] = 15;
 Wedge_18 = 'hex18'
+numberOfNodes[Wedge_18] = 18;
 Hexaedron_20 = 'hex20'
+numberOfNodes[Hexaedron_20] = 20;
+
+
+
+
+
+
 
 def CheckIntegrity():
     return "ok"
