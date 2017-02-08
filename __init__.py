b15115f944f82ba5febe08f66d1f79e3bc920fa2
Felipe Bordeu <Felipe.Bordeu@safran.fr>
2017-02-08 11:28:49 +0100

    Cleaning

--- __init__.py
+++ __init__.py
@@ -1,17 +1,12 @@
 #
 
-import Helpers
-
 __all__ = ["Helpers","IO","FE","T","Numerical"];
-#__all__ = ["FE","IO", "RM", "T","Helpers"];
-
 
 
 if __name__ == '__main__':# pragma: no cover
-    print(" OTTools Python Modules.")
-    print(" Safran All Right Reserved. 2016")    
-    print("")    
-    
+    print(" Cam Tools Python Modules.")
+    print(" Safran All Right Reserved. 2016")
+    print("")
+
     import OTTools.Helpers.Tests
     OTTools.Helpers.Tests.TestAll(extraToolsBoxs= ["OTTools"])
-    
