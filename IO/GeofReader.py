cd370e2cb5116a606dca45d904878a47932e3d1b
Felipe Bordeu <Felipe.Bordeu@safrangroup.com>
2017-09-04 11:52:04 +0200

    add metadata reading for geof files

--- IO/GeofReader.py
+++ IO/GeofReader.py
@@ -40,6 +40,232 @@ PermutationZSetToBasicTools["q8"] = [0, 2, 4, 6, 1, 3, 5, 7]
 PermutationZSetToBasicTools["c3d15"] = [0, 2, 4, 9, 11, 13, 1, 3, 5, 10, 12, 14, 6, 7, 8]
 
 
+nbIntegrationsPoints = {}
+nbIntegrationsPoints["b2d3"] = 3;
+nbIntegrationsPoints["b2d3r"] = -1;
+nbIntegrationsPoints["bn2d3"] = 3;
+nbIntegrationsPoints["bn3d4"] = 4;
+nbIntegrationsPoints["bn3d6"] = 6;
+nbIntegrationsPoints["bn3d8"] = 8;
+nbIntegrationsPoints["c1d2"] = 3;
+nbIntegrationsPoints["c1d2r"] = 1;
+nbIntegrationsPoints["c1d3"] = -1;
+nbIntegrationsPoints["c1d3r"] = -1;
+nbIntegrationsPoints["c2d3"] = 3;
+nbIntegrationsPoints["c2d3b"] = 3;
+nbIntegrationsPoints["c2d3gp16"] = 16;
+nbIntegrationsPoints["c2d3gp25"] = 25;
+nbIntegrationsPoints["c2d3gp36"] = 36;
+nbIntegrationsPoints["c2d3gp49"] = 49;
+nbIntegrationsPoints["c2d3gp64"] = 64;
+nbIntegrationsPoints["c2d3gp81"] = 81;
+nbIntegrationsPoints["c2d3gp9"] = 9;
+nbIntegrationsPoints["c2d3p3"] = 16;
+nbIntegrationsPoints["c2d3r"] = 1;
+nbIntegrationsPoints["c2d4"] = 4;
+nbIntegrationsPoints["c2d4gp12"] = 12;
+nbIntegrationsPoints["c2d4gp13"] = 13;
+nbIntegrationsPoints["c2d4gp64"] = 64;
+nbIntegrationsPoints["c2d4r"] = 1;
+nbIntegrationsPoints["c2d6"] = 6;
+nbIntegrationsPoints["c2d6gp16"] = 16;
+nbIntegrationsPoints["c2d6gp25"] = 25;
+nbIntegrationsPoints["c2d6gp36"] = 36;
+nbIntegrationsPoints["c2d6gp4"] = 4;
+nbIntegrationsPoints["c2d6gp64"] = 64;
+nbIntegrationsPoints["c2d6gp81"] = 81;
+nbIntegrationsPoints["c2d6gp9"] = 9;
+nbIntegrationsPoints["c2d6r"] = 3;
+nbIntegrationsPoints["c2d8"] = 9;
+nbIntegrationsPoints["c2d8gp12"] = 12;
+nbIntegrationsPoints["c2d8gp13"] = 13;
+nbIntegrationsPoints["c2d8gp64"] = 64;
+nbIntegrationsPoints["c2d8r"] = 4;
+nbIntegrationsPoints["c2dgp49"] = -1;
+nbIntegrationsPoints["c3d10"] = 5;
+nbIntegrationsPoints["c3d10_11"] = 11;
+nbIntegrationsPoints["c3d10_4"] = 4;
+nbIntegrationsPoints["c3d10r"] = 1;
+nbIntegrationsPoints["c3d12"] = 12;
+nbIntegrationsPoints["c3d12f"] = 18;
+nbIntegrationsPoints["c3d12l"] = -1;
+nbIntegrationsPoints["c3d12r"] = 6;
+nbIntegrationsPoints["c3d13"] = 27;
+nbIntegrationsPoints["c3d13faux"] = 5;
+nbIntegrationsPoints["c3d13r"] = 5;
+nbIntegrationsPoints["c3d15"] = 18;
+nbIntegrationsPoints["c3d15_9"] = 9;
+nbIntegrationsPoints["c3d15l"] = 0;
+nbIntegrationsPoints["c3d15r"] = 6;
+nbIntegrationsPoints["c3d16"] = 18;
+nbIntegrationsPoints["c3d16f"] = 27;
+nbIntegrationsPoints["c3d16l"] = 0;
+nbIntegrationsPoints["c3d16r"] = 8;
+nbIntegrationsPoints["c3d20"] = 27;
+nbIntegrationsPoints["c3d20l"] = 0;
+nbIntegrationsPoints["c3d20r"] = 8;
+nbIntegrationsPoints["c3d4"] = 4;
+nbIntegrationsPoints["c3d4_11"] = 1;
+nbIntegrationsPoints["c3d4_35"] = 1;
+nbIntegrationsPoints["c3d4b"] = 4;
+nbIntegrationsPoints["c3d4r"] = 1;
+nbIntegrationsPoints["c3d5"] = 5;
+nbIntegrationsPoints["c3d5_27"] = 27;
+nbIntegrationsPoints["c3d5_6"] = 6;
+nbIntegrationsPoints["c3d6"] = 6;
+nbIntegrationsPoints["c3d6l"] = -1;
+nbIntegrationsPoints["c3d6r"] = 2;
+nbIntegrationsPoints["c3d8"] = 8;
+nbIntegrationsPoints["c3d8l"] = -1;
+nbIntegrationsPoints["c3d8r"] = 1;
+nbIntegrationsPoints["cax3"] = 3;
+nbIntegrationsPoints["cax3b"] = 3;
+nbIntegrationsPoints["cax3gp16"] = 16;
+nbIntegrationsPoints["cax3gp25"] = 25;
+nbIntegrationsPoints["cax3gp36"] = 36;
+nbIntegrationsPoints["cax3gp4"] = 4;
+nbIntegrationsPoints["cax3gp49"] = 49;
+nbIntegrationsPoints["cax3gp64"] = 64;
+nbIntegrationsPoints["cax3gp81"] = 81;
+nbIntegrationsPoints["cax3gp9"] = 9;
+nbIntegrationsPoints["cax3r"] = 1;
+nbIntegrationsPoints["cax4"] = 4;
+nbIntegrationsPoints["cax4gp12"] = 12;
+nbIntegrationsPoints["cax4gp13"] = 13;
+nbIntegrationsPoints["cax4gp64"] = 64;
+nbIntegrationsPoints["cax4r"] = 1;
+nbIntegrationsPoints["cax6"] = 6;
+nbIntegrationsPoints["cax6gp16"] = -1;
+nbIntegrationsPoints["cax6gp25"] = -1;
+nbIntegrationsPoints["cax6gp36"] = -1;
+nbIntegrationsPoints["cax6gp49"] = -1;
+nbIntegrationsPoints["cax6gp64"] = -1;
+nbIntegrationsPoints["cax6gp81"] = -1;
+nbIntegrationsPoints["cax6gp9"] = -1;
+nbIntegrationsPoints["cax6r"] = 3;
+nbIntegrationsPoints["cax8"] = 9;
+nbIntegrationsPoints["cax8gp12"] = 12;
+nbIntegrationsPoints["cax8gp13"] = 13;
+nbIntegrationsPoints["cax8gp64"] = 64;
+nbIntegrationsPoints["cax8r"] = 4;
+nbIntegrationsPoints["ct2d3"] = 0;
+nbIntegrationsPoints["ct3d4"] = 0;
+nbIntegrationsPoints["cyl2"] = 3;
+nbIntegrationsPoints["cyl2r"] = 1;
+nbIntegrationsPoints["deb2"] = -1;
+nbIntegrationsPoints["dummy2"] = -1;
+nbIntegrationsPoints["dummy21"] = -1;
+nbIntegrationsPoints["dummy22"] = -1;
+nbIntegrationsPoints["dummy23"] = -1;
+nbIntegrationsPoints["dummy24"] = -1;
+nbIntegrationsPoints["dummy25"] = -1;
+nbIntegrationsPoints["dummy26"] = -1;
+nbIntegrationsPoints["dummy3"] = -1;
+nbIntegrationsPoints["dummy31"] = -1;
+nbIntegrationsPoints["dummy32"] = -1;
+nbIntegrationsPoints["dummy33"] = -1;
+nbIntegrationsPoints["dummy34"] = -1;
+nbIntegrationsPoints["dummy35"] = -1;
+nbIntegrationsPoints["dummy36"] = -1;
+nbIntegrationsPoints["gen10"] = -1;
+nbIntegrationsPoints["gen11"] = -1;
+nbIntegrationsPoints["gen12"] = -1;
+nbIntegrationsPoints["gen13"] = -1;
+nbIntegrationsPoints["gen14"] = -1;
+nbIntegrationsPoints["gen15"] = -1;
+nbIntegrationsPoints["gen2"] = -1;
+nbIntegrationsPoints["gen3"] = -1;
+nbIntegrationsPoints["gen3dX"] = -1;
+nbIntegrationsPoints["gen4"] = -1;
+nbIntegrationsPoints["gen5"] = -1;
+nbIntegrationsPoints["gen6"] = -1;
+nbIntegrationsPoints["gen7"] = -1;
+nbIntegrationsPoints["gen8"] = -1;
+nbIntegrationsPoints["gen9"] = -1;
+nbIntegrationsPoints["i2d4"] = 2;
+nbIntegrationsPoints["i2d4r"] = 1;
+nbIntegrationsPoints["i3d12"] = 6;
+nbIntegrationsPoints["i3d12r"] = 3;
+nbIntegrationsPoints["i3d6"] = 3;
+nbIntegrationsPoints["i3d8"] = 4;
+nbIntegrationsPoints["i3d8r"] = 1;
+nbIntegrationsPoints["iax4"] = 2;
+nbIntegrationsPoints["iax4r"] = 1;
+nbIntegrationsPoints["l2d2"] = -1;
+nbIntegrationsPoints["l2d2r"] = -1;
+nbIntegrationsPoints["l2d3"] = 1;
+nbIntegrationsPoints["l2d3r"] = 1;
+nbIntegrationsPoints["l3d1"] = -1;
+nbIntegrationsPoints["l3d1r"] = -1;
+nbIntegrationsPoints["l3d2"] = 1;
+nbIntegrationsPoints["l3d2r"] = 1;
+nbIntegrationsPoints["l3d3"] = 1;
+nbIntegrationsPoints["l3d3r"] = 1;
+nbIntegrationsPoints["lag1"] = -1;
+nbIntegrationsPoints["m3d3"] = -1;
+nbIntegrationsPoints["m3d3r"] = -1;
+nbIntegrationsPoints["m3d4"] = -1;
+nbIntegrationsPoints["m3d4r"] = -1;
+nbIntegrationsPoints["m3d6"] = -1;
+nbIntegrationsPoints["m3d6r"] = -1;
+nbIntegrationsPoints["m3d8"] = -1;
+nbIntegrationsPoints["m3d8r"] = -1;
+nbIntegrationsPoints["r2d2"] = 0;
+nbIntegrationsPoints["r3d2"] = 0;
+nbIntegrationsPoints["rve1d"] = -1;
+nbIntegrationsPoints["rve2d"] = -1;
+nbIntegrationsPoints["rve3d"] = -1;
+nbIntegrationsPoints["s3d4"] = 8;
+nbIntegrationsPoints["s3d6r"] = 6;
+nbIntegrationsPoints["s3d8r"] = 8;
+nbIntegrationsPoints["sax2"] = 3;
+nbIntegrationsPoints["sax2r"] = 1;
+nbIntegrationsPoints["sax2rr"] = -1;
+nbIntegrationsPoints["sax3"] = -1;
+nbIntegrationsPoints["sax3r"] = -1;
+nbIntegrationsPoints["sax3rr"] = -1;
+nbIntegrationsPoints["skin2d3"] = 3;
+nbIntegrationsPoints["skin3d4"] = 4;
+nbIntegrationsPoints["skin3d4r"] = 1;
+nbIntegrationsPoints["skin3d6"] = 6;
+nbIntegrationsPoints["skin3d6r"] = 3;
+nbIntegrationsPoints["skin3d8r"] = 4;
+nbIntegrationsPoints["skinax3"] = 3;
+nbIntegrationsPoints["sph2"] = 3;
+nbIntegrationsPoints["sph2r"] = 1;
+nbIntegrationsPoints["spr1"] = 0;
+nbIntegrationsPoints["spr1_2d"] = 0;
+nbIntegrationsPoints["spr1_3d"] = 0;
+nbIntegrationsPoints["spr2"] = -1;
+nbIntegrationsPoints["x2d3"] = 3;
+nbIntegrationsPoints["x2d3r"] = 1;
+nbIntegrationsPoints["x2d4"] = 4;
+nbIntegrationsPoints["x2d4r"] = 1;
+nbIntegrationsPoints["x2d6"] = 6;
+nbIntegrationsPoints["x2d6r"] = 3;
+nbIntegrationsPoints["x2d8"] = 9;
+nbIntegrationsPoints["x2d8r"] = 4;
+nbIntegrationsPoints["x3d10"] = 5;
+nbIntegrationsPoints["x3d10r"] = 1;
+nbIntegrationsPoints["x3d15"] = 18;
+nbIntegrationsPoints["x3d15r"] = 6;
+nbIntegrationsPoints["x3d20"] = 27;
+nbIntegrationsPoints["x3d20r"] = 8;
+nbIntegrationsPoints["x3d4"] = 4;
+nbIntegrationsPoints["x3d4r"] = 1;
+nbIntegrationsPoints["x3d6"] = 6;
+nbIntegrationsPoints["x3d6r"] = 2;
+nbIntegrationsPoints["x3d8"] = 8;
+nbIntegrationsPoints["x3d8r"] = 1;
+nbIntegrationsPoints["xax3"] = 3;
+nbIntegrationsPoints["xax3r"] = 1;
+nbIntegrationsPoints["xax4"] = 4;
+nbIntegrationsPoints["xax4r"] = 1;
+nbIntegrationsPoints["xax6"] = 6;
+nbIntegrationsPoints["xax6r"] = 3;
+nbIntegrationsPoints["xax8"] = 9;
+nbIntegrationsPoints["xax8r"] = 4;
+
 def ReadGeof(fileName=None,string=None,out=None):
     reader = GeofReader()
     reader.SetFileName(fileName)
@@ -52,6 +278,40 @@ class GeofReader(ReaderBase):
         super(GeofReader,self).__init__()
         self.commentChar= "%"
         self.readFormat = 'r'
+
+  def ReadMetaData(self):
+    res = {}
+    self.StartReading()
+    nbIP  = 0
+    l = self.ReadCleanLine()
+
+    while(True):
+      if l == None :
+          break
+      if l[0] != "*":
+          pass
+      elif l.find("**node")>-1:
+        l       = self.ReadCleanLine()
+        s       = l.split()
+        res["nbNodes"] = int(s[0])
+      elif l.find("**element")>-1:
+        l  = self.ReadCleanLine()
+        res['nbElements'] = int(l.split()[0])
+        nbIntegrationsPoints
+        while(True):
+          l  = self.ReadCleanLine()
+          if l.find("**") > -1:
+               break
+          s = l.split()
+          nbIP += nbIntegrationsPoints[s[1]]
+        continue
+      l = self.ReadCleanLine()
+
+    res['nbIntegrationPoints'] = nbIP
+
+    self.EndReading()
+
+    return res
 
 
   def Read(self, fileName=None,string=None,out=None):
@@ -70,7 +330,6 @@ class GeofReader(ReaderBase):
     else:
         res = out
 
-
     filetointernalid = {}
 
     oidToElementContainer = {}
@@ -82,8 +341,6 @@ class GeofReader(ReaderBase):
       if l is None:
           print("ERROR premature EOF: please check the integrity of your geof file") # pragma: no cover
           break # pragma: no cover
-
-
       #if len(l) == 0: l = string.readline().strip('\n').lstrip().rstrip(); continue
 
       if l.find("**node")>-1:
--- IO/ReaderBase.py
+++ IO/ReaderBase.py
@@ -16,6 +16,7 @@ class ReaderBase(BaseOutputObject):
         self.output = None
         self.string = None
         self.commentChar = None
+        self.filePointer = None
 
     def StartReading(self):
 
@@ -42,7 +43,7 @@ class ReaderBase(BaseOutputObject):
             self.__path = None;
             self.string = None;
         else:
-            self.filePath = os.path.abspath(os.path.dirname(fileName));
+            self.filePath = os.path.abspath(os.path.dirname(fileName))+os.sep;
             self.string = None
 
 
