545fc74e7a3b53179b7e6a91d1b50578ae157ffe
Felipe Bordeu <Felipe.Bordeu@safrangroup.com>
2017-09-25 10:28:32 +0200

    more options on the OdbWriter

--- IO/OdbWriter.py
+++ IO/OdbWriter.py
@@ -12,15 +12,20 @@ import BasicTools.FE.ElementNames as EN
 OdbName = {}
 OdbName[EN.Tetrahedron_4] = 'C3D4'
 OdbName[EN.Triangle_3] = 'S3'
+#OdbName[EN.Triangle_3] = 'S3R'
 OdbName[EN.Bar_2] = 'CONN3D2'
 Abaqusexec = "/home/software/abaqus/Commands/abq6136"
 
 def WriteMaterial(odb, material):
     import abaqusConstants as AC
 
-    #Creating material for odb
-    pMat = odb.Material(name='MATERIAL')
-    pMat.Elastic(type=AC.ISOTROPIC,table=((12000,0.3),))
+    if material is none:
+        #Creating material for odb
+        pMat = odb.Material(name='Elastic Material')
+        pMat.Elastic(type=AC.ISOTROPIC,
+            temperatureDependency=OFF, dependencies=0,
+            noCompression=OFF, noTension=OFF,
+            moduli=LONG_TERM, table=((12000,0.3),))
     print("print Material")
 
 def WriteSection(odb, section):
@@ -31,7 +36,27 @@ def WriteSection(odb, section):
     material = 'MATERIAL',
     thickness = 1.0)
 
-def WriteOdb(filename,mesh, __insubprocess= False):
+def WriteOdb(filename,mesh,PointFields=None,CellFields=None,PointFieldsNames=None,CellFieldsNames=None,  __insubprocess= False):
+
+
+    if PointFields is None:
+        PointFields = [];
+
+    if CellFields  is None:
+        CellFields   = [];
+
+#    if GridFields is None:
+#        GridFields  = [];
+
+    if PointFieldsNames is None:
+        PointFieldsNames  = [];
+
+    if CellFieldsNames is None:
+        CellFieldsNames  = [];
+
+#    if GridFieldsNames is None:
+#        GridFieldsNames  = [];
+
 
     try :
         import abaqusConstants as AC
@@ -57,7 +82,7 @@ def WriteOdb(filename,mesh, __insubprocess= False):
         interface.processDirectory = TestTempDir.GetTempPath()
         absfilename   = os.path.abspath(filename)
 
-        port = 12335
+        port = 12346
         interface.tpl = """
 from BasicTools.IO.Wormhole import WormholeServer
 WormholeServer(""" +str(port) +""",dry=False)
@@ -83,8 +108,12 @@ WormholeServer(""" +str(port) +""",dry=False)
         print("Writing file :")
         print(absfilename)
         client.SendData("mesh",mesh)
+        client.SendData("PointFields",PointFields)
+        client.SendData("CellFields",CellFields)
+        client.SendData("PointFieldsNames",PointFieldsNames)
+        client.SendData("CellFieldsNames",CellFieldsNames)
         client.RemoteExec("from BasicTools.IO.OdbWriter import WriteOdb")
-        client.RemoteExec("WriteOdb(filename,mesh)")
+        client.RemoteExec("WriteOdb(filename,mesh,PointFields,CellFields,PointFieldsNames,CellFieldsNames)")
         client.RemoteExec("print('Done')")
         client.Exit()
 
@@ -112,7 +141,7 @@ WormholeServer(""" +str(port) +""",dry=False)
 #
 ##Creating the 3D solid part
     pPart = pOdb.Part(name='beamTaylor',embeddedSpace = AC.THREE_D,type= AC.DEFORMABLE_BODY)
-    nodeLabels = list(range(mesh.GetNumberOfNodes()))
+    nodeLabels = list(range(1,1+mesh.GetNumberOfNodes()))
     pPart.addNodes(labels = nodeLabels,coordinates = mesh.GetPosOfNodes())
 
 
@@ -129,38 +158,16 @@ WormholeServer(""" +str(port) +""",dry=False)
     cpt =0;
     for ntype, data in mesh.elements.items():
         elemtype = OdbName[ntype]
-        print(elemtype)
-        #if EN.dimension[ntype] != maxDimensionalityOfelements and lowerDimElementsAsSets:
-        #    continue
-        #npe = data.GetNumberOfNodesPerElement()
-        #if elemtype!="c2d3":
-        #print(data.connectivity)
-        #print(map(tuple,data.connectivity.astype(np.int32)))
-        dd = list(range(data.globaloffset,data.globaloffset+data.GetNumberOfElements()))
-        print("------------------")
+
+        dd = list(range(data.globaloffset+1,data.globaloffset+1+data.GetNumberOfElements()))
         dd = np.array(dd,dtype=np.int32)
-        #print(dd)
-        #print(data.connectivity.astype(np.int32))
-        #ddd = np.hstack((dd, data.connectivity))
+        print(dd)
+        print(data.connectivity.astype(np.int32))
+        print(elemtype)
+        print(ntype)
 
-
-        #pPart.addElements(elementData = map(tuple,ddd.astype(np.int32)) , type = elemtype)
         pPart.addElements(labels = dd , connectivity=data.connectivity.astype(np.int32) , type = elemtype, elementSetName=ntype)
-        #pPart.addElements(elementData = data.connectivity.astype(INT), type = elemtype)
-        #,elementSetName = 'ELSETPART'
-#,sectionCategory=pCat
-        #for i in range(data.GetNumberOfElements() ):
-        #    if useOriginalId:
-        #        self.filePointer.write("{} {} ".format(data.originalIds[i],elemtype) )
-        #        self.filePointer.write(" ".join([str(int(meshObject.originalIDNodes[x])) for x in data.connectivity[i,:].ravel()]))
-        #    else:
-        #        self.filePointer.write("{} {} ".format(cpt+1,elemtype) )
-        #        self.filePointer.write(" ".join([str(x+1) for x in data.connectivity[i,:].ravel()]))
-        #    cpt += 1;
-        #    self.filePointer.write("\n")
-        #for tag in data.tags:
-        #   pPElt = pPart.elementSets[tag.name]
-        #   pPart.assignSection(region=pPElt,section=mySection )
+
 #
 
     for name in mesh.GetNamesOfElemTags():
@@ -176,17 +183,36 @@ WormholeServer(""" +str(port) +""",dry=False)
 ##Creating the analysis step
     pStep = pOdb.Step(name='StaticAnalysis',description='Analysis type - 101',domain=AC.TIME,timePeriod = 1.0)
     pFrame0 = pStep.Frame(incrementNumber=0,frameValue=0.0000)
-    pDisp0 = pFrame0.FieldOutput(name='U',description='Displace ment',type=AC.VECTOR,componentLabels=('1','2','3'))
+    #pDisp0 = pFrame0.FieldOutput(name='U',description='Displace ment',type=AC.VECTOR,componentLabels=('1','2','3'))
 
-    dispValues = np.hstack((np.array(nodeLabels)[:,np.newaxis],)*3).ravel().astype(float)
-    dispValues.shape = (len(nodeLabels),3)
-    dispValues /= len(nodeLabels)*10
+    #dispValues = np.hstack((np.array(nodeLabels)[:,np.newaxis],)*3).ravel().astype(float)
+    #dispValues.shape = (len(nodeLabels),3)
+    #dispValues /= len(nodeLabels)*10
 
-    pDisp0.addData(position = AC.NODAL,instance = pAssembly,labels = nodeLabels,data=dispValues)
+    #pDisp0.addData(position = AC.NODAL,instance = pAssembly,labels = nodeLabels,data=dispValues)
 
 
 
-    #sessionstep=pOdb.step(name='bla', desription='', domain=TIME, timePeriod=1.0)
+    cellLabels = range(1,1+mesh.GetNumberOfElements())
+    for i in range(len(CellFieldsNames)):
+        if mesh.GetNumberOfElements() == CellFields[i].size:
+            ftype = AC.SCALAR
+        elif mesh.GetNumberOfElements()*3 == CellFields[i].size:
+            ftype = AC.VECTOR
+        else:
+            raise
+        fo = pFrame0.FieldOutput(name=CellFieldsNames[i],description=CellFieldsNames[i],type=ftype)
+        fo.addData(position = AC.CENTROID,instance = pAssembly,labels = cellLabels,data=CellFields[i])
+
+    for i in range(len(PointFieldsNames)):
+        if mesh.GetNumberOfNodes() == PointFields[i].size:
+            ftype = AC.SCALAR
+        elif mesh.GetNumberOfNodes()*3 == PointFields[i].size:
+            ftype = AC.VECTOR
+        else:
+            raise
+        fo = pFrame0.FieldOutput(name=PointFieldsNames[i],description=PointFieldsNames[i],type=ftype )
+        fo.addData(position = AC.NODAL,instance = pAssembly,labels = nodeLabels,data=PointFields[i])
 
 
 ##Creating the frame for the step
