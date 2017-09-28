dc17249acec66c9fb293df7360c91a9fe4e763f9
Felipe Bordeu <Felipe.Bordeu@safrangroup.com>
2017-09-28 10:49:16 +0200

    work on a cleaner code

--- IO/FemReader.py
+++ IO/FemReader.py
@@ -11,6 +11,17 @@ from BasicTools.IO.ReaderBase import ReaderBase
 import BasicTools.FE.ElementNames as ElementNames
 
 
+def ReadFem(fileName=None,string=None):
+    obj = FemReader()
+    if fileName is not None:
+        obj.SetFileName(fileName)
+
+    if string is not None:
+        obj.SetStringToRead(string)
+
+    return obj.Read()[0]
+
+
 class FemReader(ReaderBase):
     def __init__(self,fileName = None):
         super(FemReader,self).__init__()
@@ -27,35 +38,47 @@ class FemReader(ReaderBase):
         zs = []
         filetointernalid = {}
         cpt =0
+
         while(True):
             line = self.ReadCleanLine()
+            if line is None:
+                break
 
-            if line[0:8] == 'DESGLB  ' : continue
-            if line[0:8] == 'SUBCASE ' : continue
-            if line[0:8] == 'ANALYSIS' : continue
-            if line[0:8] == '  SPC = ' : continue
-            if line[0:8] == '  LOAD =' : continue
-            if line[0:8] == 'DESOBJ(M' : continue
-            if line[0:8] == '  LABEL ' : continue
+            l = line.split()
+            key = l[0]
+
+            if key == 'DESGLB' : continue
+            if key == 'SUBCASE' : continue
+            if key == 'ANALYSIS' : continue
+            if key == 'SPC' : continue
+            if key == 'LOAD' : continue
+            if line[0:6] == 'DESOBJ' : continue
+            if key == 'LABEL' : continue
             if line == "BEGIN BULK" :
                 while(True):
                     line = self.ReadCleanLine()
-                    if line[0:8] == 'DOPTPRM ' : continue
-                    if line[0:8] == 'DTPL    ' : continue
-                    if line[0:8] == 'DCONSTR ' : continue
-                    if line[0:8] == 'DCONADD ' : continue
-                    if line[0:8] == 'DRESP1  ' : continue
-                    if line[0:8] == 'PARAM,CH' : continue
-                    if line[0:8] == 'CORD2R  ' : continue
-                    if line[0:8] == '+       ' : continue
-                    if line[0:6] == 'ENDATA' : break
-                    if line[0:8] == 'PSOLID  ' : continue
-                    if line[0:8] == 'MAT1    ' : continue
-                    if line[0:8] == 'SPC     ' :
+                    if line is None:
+                        break
+
+                    l = line.split()
+                    key = l[0]
+                    if key == 'DOPTPRM' : continue
+                    if key == 'DTPL' : continue
+                    if key == 'DCONSTR' : continue
+                    if key == 'DCONADD' : continue
+                    if key == 'DRESP1' : continue
+                    if line[0:5] == 'PARAM' : continue
+                    if key == 'CORD2R' : continue
+                    if key == '+' : continue
+                    if key == 'ENDATA' : break
+                    if key == 'PSOLID' : continue
+                    if key == 'MAT1' : continue
+
+                    if l[0] == 'SPC' :
                         tag = res.GetNodalTag("SPC"+str(int(line[8:8*2]) ))
                         tag.AddToTag(filetointernalid[int(line[8*2:8*3]) ])
                         continue
-                    if line[0:8] == 'FORCE   ' :
+                    if key == 'FORCE' :
                         name = "FORCE"+str(int(line[8:8*2]) )
                         tag = res.GetNodalTag(name)
                         node = filetointernalid[int(line[8*2:8*3]) ]
@@ -67,8 +90,8 @@ class FemReader(ReaderBase):
                         val = [fx,fy,fz]
                         resdic[name] = {'N':node, "factor":ParseFloat(line[8*3:8*4]), "val":val }
                         continue
-                    if line[0:8] == 'GRID    ' :
-                      #print(line)
+                    if key == 'GRID' :
+                      line += "             "
                       oid = int(line[8:8*2])
                       x = ParseFloat(line[8*3:8*4])
                       y = ParseFloat(line[8*4:8*5])
@@ -81,7 +104,7 @@ class FemReader(ReaderBase):
                       cpt +=1
 
                       continue
-                    if line[0:8] ==  'CTETRA  ':
+                    if key ==  'CTETRA':
                       oid = int(line[8:8*2])
                       idd2 = str(int(line[8*2:8*3]))
                       P1 = int(line[8*3:8*4])
@@ -99,11 +122,12 @@ class FemReader(ReaderBase):
                       continue
 
                     print("string '" + str(line) + "' not treated")
-                    break
-                break
-            if line[0:6] == 'ENDATA' : break
+
+
+            if key == 'ENDATA' : break
             print("string '" + str(line) + "' not treated")
-            break
+            if line is None: break
+            raise
 
         res.nodes = np.array([xs,ys,zs],dtype=np.float).T
         res.originalIDNodes = np.array(ids,dtype=np.int)
@@ -127,15 +151,7 @@ def ParseFloat(line):
 
     return float(line)
 
-def ReadFem(fileName=None,string=None):
-    obj = FemReader()
-    if fileName is not None:
-        obj.SetFileName(fileName)
 
-    if string is not None:
-        obj.SetStringToRead(string)
-
-    return obj.Read()[0]
 
 def CheckIntegrity(GUI = False):
 
