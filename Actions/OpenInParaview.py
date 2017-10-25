8dc5c33bb8bb1dc17ce622c957882381858a6f45
Felipe Bordeu <Felipe.Bordeu@safrangroup.com>
2017-10-25 14:05:30 +0200

    Open In Paraview with an already created file

--- Actions/OpenInParaview.py
+++ Actions/OpenInParaview.py
@@ -1,27 +1,27 @@
 # -*- coding: utf-8 -*-
 
 from BasicTools.Helpers.Tests import TestTempDir
+from BasicTools.IO.PathControler import PathControler as PC
 
 paraviewExec= "paraview"
-def OpenInParaView( mesh,filename=None ):
+def OpenInParaView( mesh=None,filename=None ):
 
     if filename is None:
         from BasicTools.Helpers.Tests import GetUniqueTempFile
         (fd,filename) = GetUniqueTempFile(suffix=".xmf",prefix="ExportedDataBasictools_")
     else:
-        tempdir = TestTempDir.GetTempPath()
-        filename = tempdir + filename
+        filename = PC.GetFullFilenameOnTempDirectory(filename)
+        #tempdir = TestTempDir.GetTempPath()
+        #filename = tempdir + filename
 
+    if mesh is not None:
+        from BasicTools.IO.XdmfWriter import WriteMeshToXdmf
+        PointFieldsNames = mesh.nodeFields.keys()
+        PointFields =  mesh.nodeFields.values()
+        CellFieldsNames = mesh.elemFields.keys()
+        CellFields = mesh.elemFields.values()
 
-    from BasicTools.IO.XdmfWriter import WriteMeshToXdmf
-
-
-    PointFieldsNames = mesh.nodeFields.keys()
-    PointFields =  mesh.nodeFields.values()
-    CellFieldsNames = mesh.elemFields.keys()
-    CellFields = mesh.elemFields.values()
-
-    WriteMeshToXdmf(filename,mesh,PointFieldsNames=PointFieldsNames,PointFields=PointFields,CellFieldsNames=CellFieldsNames,CellFields=CellFields  )
+        WriteMeshToXdmf(filename,mesh,PointFieldsNames=PointFieldsNames,PointFields=PointFields,CellFieldsNames=CellFieldsNames,CellFields=CellFields  )
 
     from subprocess import Popen
     Popen([paraviewExec,filename])
