ee06c420467f8fb336bcb66fd4cc7ca4a2021ca1
Felipe Bordeu <felipe.bordeu@safrangroup.com>
2018-10-17 22:07:55 +0200

    Capability to write files without executing ParaView

--- Actions/OpenInParaView.py
+++ Actions/OpenInParaView.py
@@ -4,7 +4,7 @@ from BasicTools.Helpers.Tests import TestTempDir
 from BasicTools.IO.PathControler import PathControler as PC
 
 paraviewExec= "paraview"
-def OpenInParaView( mesh=None,filename=None ):
+def OpenInParaView( mesh=None,filename=None, run=True):
 
     if filename is None:
         from BasicTools.Helpers.Tests import GetUniqueTempFile
@@ -23,8 +23,9 @@ def OpenInParaView( mesh=None,filename=None ):
 
         WriteMeshToXdmf(filename,mesh,PointFieldsNames=PointFieldsNames,PointFields=PointFields,CellFieldsNames=CellFieldsNames,CellFields=CellFields  )
 
-    from subprocess import Popen
-    Popen([paraviewExec,filename])
+    if run:
+        from subprocess import Popen
+        Popen([paraviewExec,filename])
 
 def CheckIntegrity():
     from BasicTools.Actions.OpenInParaView import OpenInParaView
