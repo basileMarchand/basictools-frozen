e60671d18fbe334bceac23bbcd3dfca090750e5f
d582428 <fabien.casenave@safrangroup.com>
2019-07-12 16:53:01 +0200

    add ReadVariableFromPythonFile function

--- Helpers/GlobalFunctions.py
+++ Helpers/GlobalFunctions.py
@@ -6,3 +6,15 @@ def TryCommand(string):
     os.system(string)
   except:
     True 
+
+
+def ReadVariableFromPythonFile(fileName):
+  dic = {}
+  with open (fileName, "r") as myfile:
+    data = myfile.readlines()
+  for d in data:
+    try:
+      exec(d, dic, dic)
+    except SyntaxError:
+      continue
+  return dic
