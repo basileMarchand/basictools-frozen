9e74841ab4d8accfddecb5088c69b786a41a9e17
d582428 <fabien.casenave@safrangroup.com>
2019-08-31 20:01:26 +0200

    Add various Assert tests in Helpers/GlobalFunctions

--- Helpers/GlobalFunctions.py
+++ Helpers/GlobalFunctions.py
@@ -29,10 +29,45 @@ def GetIterable(x):
     else:
         return (x,)
 
+
 def AssertFloat(value):
     arguments = GetIterable(value)
     for arg in arguments:
+        if type(arg) == np.ndarray:
+            arg = arg.flat[0]
         assert (type(arg) == float or type(arg) == np.float64), "value is not of type float or np.float64"
+    return
+
+
+def AssertString(value):
+    arguments = GetIterable(value)
+    for arg in arguments:
+        if type(arg) == np.ndarray:
+            arg = arg.flat[0]
+        assert (type(arg) == str), "value is not of type string"
+    return
+
+
+def AssertType(myType, value):
+    """
+    Check of all the elements of "value" are of any of the types in "myType"
+    myType and value can be singletons or iterables
+    """
+    values = GetIterable(value)
+    types = GetIterable(myType)
+    for val in values:
+        answer = []
+        for typ in types:
+            answer.append(type(val) == typ)
+        assert (True in answer), str(value)+" is not of any of the types "+str(types)
+    return
+
+
+def AssertSameLength(value):
+    arguments = GetIterable(value)
+    length0 = np.array(arguments[0]).flatten().shape[0]
+    for i, arg in enumerate(arguments):
+        assert (np.array(arguments[i]).flatten().shape[0] == length0), "values do not have same length"
     return
 
 
@@ -42,6 +77,11 @@ def CheckIntegrity():
     AssertFloat(1.)
     AssertFloat((1., 2.))
     AssertFloat([1., 2.])
+    AssertFloat(np.array([1., 2.]))
+    AssertString(("A", "toto"))
+    AssertType([str, float], ("A", "toto"))
+
+    AssertSameLength((np.array([1., 2.]), [3., 4]))
 
     import BasicTools.TestData as BasicToolsTestData
     ReadVariableFromPythonFile(BasicToolsTestData.GetTestDataPath()+"test.py")
