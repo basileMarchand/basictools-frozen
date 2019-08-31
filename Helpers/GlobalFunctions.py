930a9b6002770457c583d6ce37da3ccb854b322e
d582428 <fabien.casenave@safrangroup.com>
2019-08-31 10:43:43 +0200

    correct AssertFloat in Helpers/GlobalFunctions.py

--- Helpers/GlobalFunctions.py
+++ Helpers/GlobalFunctions.py
@@ -1,41 +1,52 @@
 import os
 import numpy as np
+import collections
 
 
 def TryCommand(string):
-  try:
-    os.system(string)
-  except:
-    True 
+    try:
+        os.system(string)
+    except:
+        True 
 
 
 def ReadVariableFromPythonFile(fileName):
-  dic = {}
-  with open (fileName, "r") as myfile:
-    data = myfile.readlines()
-  for d in data:
-    try:
-      exec(d, dic, dic)
-    except SyntaxError:
-      continue
-  dic.pop('__builtins__', None)
-  return dic
+    dic = {}
+    with open (fileName, "r") as myfile:
+        data = myfile.readlines()
+        for d in data:
+            try:
+                exec(d, dic, dic)
+            except SyntaxError:
+                continue
+    dic.pop('__builtins__', None)
+    return dic
 
 
+def GetIterable(x):
+    if isinstance(x, collections.Iterable):
+        return x
+    else:
+        return (x,)
 
 def AssertFloat(value):
-    assert (type(value) == float or type(value) == np.float64), "value is not of type float or np.float64"
+    arguments = GetIterable(value)
+    for arg in arguments:
+        assert (type(arg) == float or type(arg) == np.float64), "value is not of type float or np.float64"
+    return
 
 
 def CheckIntegrity():
 
-  TryCommand("ls")
-  AssertFloat(1.)
+    TryCommand("ls")
+    AssertFloat(1.)
+    AssertFloat((1., 2.))
+    AssertFloat([1., 2.])
 
-  import BasicTools.TestData as BasicToolsTestData
-  ReadVariableFromPythonFile(BasicToolsTestData.GetTestDataPath()+"test.py")
+    import BasicTools.TestData as BasicToolsTestData
+    ReadVariableFromPythonFile(BasicToolsTestData.GetTestDataPath()+"test.py")
 
-  return 'ok'
+    return 'ok'
 
 if __name__ == '__main__':
     CheckIntegrity() # pragma: no cover
---
+++ TestData/test.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+
+a = np.zeros(10)
+
+b = 1.
+
+d = b*a
