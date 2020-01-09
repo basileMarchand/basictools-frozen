803e674d76f565d23ede62e904fa6d97b13651f7
Julien Cortial <julien.cortial@safrangroup.com>
2020-01-09 16:45:59 +0100

    Add license information

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+#
+# This file is subject to the terms and conditions defined in
+# file 'LICENSE.txt', which is part of this source code package.
+#
+#
+
 from setuptools import setup, find_packages
 from Cython.Build import cythonize
 import numpy, os
