6fface533b1fa0763e769a5bc8a6ddbb988feced
Julien Cortial <julien.cortial@safrangroup.com>
2020-02-19 14:09:15 +0100

    (setup) Gracefully handle missing cython
    Based on a contribution from Basile Marchand (Mines ParisTech)

--- setup.py
+++ setup.py
@@ -5,71 +5,77 @@
 #
 #
 
-from setuptools import setup, find_packages
-from Cython.Build import cythonize
-import numpy, os
-from setuptools.command.build_ext import build_ext
+import os, sys
+import numpy
+import setuptools
 
+
+# Compilation options
 enable_MKL = True
 debug = False
 force = True # to force recompilation
-annotate = debug # to generate annotation (html files )
+annotate = debug # to generate annotation (HTML files)
 useOpenmp = True
 
+try:
+    from Cython.Build import cythonize
 
-# configuration for compilers
-# solution from https://stackoverflow.com/questions/30985862/how-to-identify-compiler-before-defining-cython-extensions
-BUILD_ARGS = {}
-for compiler in ['msvc','gcc',"icc"]:
-    if debug:
-        compile_args = ['-g','-O', '-std=c++11']
-    else:
-        compile_args = ['-O3', '-std=c++11']
+    # Compiler-dependent configuration
+    # See https://stackoverflow.com/questions/30985862
+    BUILD_ARGS = {}
+    for compiler in ('msvc', 'gcc', 'icc'):
+        if debug:
+            compile_args = ['-g','-O', '-std=c++11']
+        else:
+            compile_args = ['-O3', '-std=c++11']
 
-    if useOpenmp:
-        compile_args.append("-fopenmp")
-        if compiler == "icc":
-            compile_args.append("-inline-forceinline")
+        if useOpenmp:
+            compile_args.append("-fopenmp")
+            if compiler == "icc":
+                compile_args.append("-inline-forceinline")
 
+        if enable_MKL:
+            compile_args.append("-DMKL_DIRECT_CALL")
+            compile_args.append("-DEIGEN_USE_MKL_VML")
 
-    if enable_MKL:
-        #compile_args.append("-DEIGEN_USE_MKL_ALL")
-        compile_args.append("-DMKL_DIRECT_CALL")
-        compile_args.append("-DEIGEN_USE_MKL_VML")
+        BUILD_ARGS[compiler] = compile_args
 
-    BUILD_ARGS[compiler] = compile_args
+    include_path = [numpy.get_include(), os.environ['EIGEN_INC']]
+    if "CONDA_PREFIX" in os.environ:
+        include_path.append(os.environ["CONDA_PREFIX"] + "/include/")
+        modules = cythonize("src/BasicTools/FE/*.pyx", gdb_debug=debug, annotate=annotate, include_path=include_path, force=force)
+        for m in modules:
+            m.include_dirs = include_path + ["src/BasicTools"]
+            m.extra_compile_args = compile_args
+            if enable_MKL:
+                m.extra_link_args.append("-lmkl_core")
+                m.extra_link_args.append("-lmkl_avx")
+                m.extra_link_args.append("-lmkl_intel_lp64")
+                m.extra_link_args.append("-lmkl_sequential")
+                m.extra_link_args.append("-lmkl_def")
 
+except ImportError as e:
+    print(f"Compilation disabled since {e.name} package is missing")
+    modules = []
+
+from setuptools.command.build_ext import build_ext
 
 class build_ext_compiler_check(build_ext):
     def build_extensions(self):
-        compiler=os.path.basename(self.compiler.compiler[0])
+        compiler = os.path.basename(self.compiler.compiler[0])
         compiler_type = self.compiler.compiler_type
-        print(compiler)
-        print(compiler_type)
+        print(f"Using compiler {compiler} of type {compiler_type}")
         args = BUILD_ARGS[compiler]
         for ext in self.extensions:
             ext.extra_compile_args = args
         build_ext.build_extensions(self)
 
-
-include_path = [numpy.get_include(), os.environ['EIGEN_INC'] ]
-if "CONDA_PREFIX" in os.environ:
-    include_path.append(os.environ["CONDA_PREFIX"]+"/include/")
-
-modules = cythonize("src/BasicTools/FE/*.pyx", gdb_debug=debug, annotate=annotate, include_path=include_path, force=force)
-
-for m in modules:
-    m.include_dirs = include_path + ["src/BasicTools"]
-    m.extra_compile_args = compile_args
-    if enable_MKL:
-        m.extra_link_args.append("-lmkl_core")
-        m.extra_link_args.append("-lmkl_avx")
-        m.extra_link_args.append("-lmkl_intel_lp64")
-        m.extra_link_args.append("-lmkl_sequential")
-        m.extra_link_args.append("-lmkl_def")
+from setuptools import setup, find_packages
+requirements = ["numpy", "sympy"]
 
 setup(name='BasicTools',
       packages=find_packages('src'),
       ext_modules=modules,
+      install_requires=requirements,
       package_dir={'': 'src'},
-      cmdclass={ 'build_ext': build_ext_compiler_check })
+      cmdclass={'build_ext': build_ext_compiler_check})
