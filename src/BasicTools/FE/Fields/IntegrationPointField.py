e69e67522aeec1d78126690e64ee0c3a9f0eb04e
d584808 <felipe.bordeu@safrangroup.com>
2020-02-24 11:18:00 +0100

    Update IntegrationPointField.py

--- src/BasicTools/FE/Fields/IntegrationPointField.py
+++ src/BasicTools/FE/Fields/IntegrationPointField.py
@@ -12,55 +12,74 @@ from BasicTools.Helpers.BaseOutputObject import BaseOutputObject
 
 from BasicTools.Containers.UnstructuredMesh import AllElements
 
-from BasicTools.FE.IntegrationsRules import Lagrange as Lagrange
+from BasicTools.FE.IntegrationsRules import IntegrationRulesAlmanac as IntegrationRulesAlmanac
+from BasicTools.Containers import ElementNames as EN
 
 
 class IntegrationPointField(BaseOutputObject):
-    def __init__(self,name):
+    def __init__(self,name=None,mesh=None,rules=None,data=None):
         super(IntegrationPointField,self).__init__()
-        self.storage = {}
         self.name = name
-        #self.ncoomp = 1
-        pass
-    def Allocate(self,mesh,integrationrule, tag = AllElements):
-        for name,data in mesh.elements.items():
-            nbItegPoints = len(Lagrange(name)[1])
-            if tag == AllElements:
-                nbElements = data.GetNumberOfElements()
-            else :
-                if tag in data.tags:
-                    nbElements = len(data.tags[tag])
-                else:
-                    nbElements = 0
-            self.storage[name] = np.zeros((nbElements,nbItegPoints), dtype=np.float)
+        
+        if data is None:
+            self.data = {}
+        else:
+            self.data = data
+            
+        #self.data["tetra"] = numpy( nuberOfElements, numberOfIntegrationPoints)
+        self.mesh = mesh
+        self.rules = rules # dict like  [EN.GeoTri] = tuple(points, weights)
+        #self.filter = ElementFilter(dimensionality=3,tag="Toto")
+        
+    def SetRule(self,ruleName):
+        self.rules = IntegrationRulesAlmanac[ruleName]
+    
+    def GetRuleFor(self,elemtype):
+        return self.rules[EN.geoSupport[elemtype])
+        
+    def GetFieldFor(self,elemtype):
+            return self.data[elemtype]
+    
+    def Allocate(self):
+        for name,data in self.mesh.elements.items():
+            nbItegPoints = len(self.GetRuleFor(name)[1])
+            nbElements = data.GetNumberOfElements()
+            self.data[name] = np.zeros((nbElements,nbItegPoints), dtype=np.float)
 
+    def __binaryOp__(self,other,op):
+        self.__CheckCompatiblility(self,other)
+        
+        res = IntegrationPointField(name = "" )
+        res.data = { key:op(A.data,B.data) for A,B in zip(self.data,other.data)}
+        res.mesh = self.mesh
+        res.rules = self.rules
+        return res
+
+    def __add__(self,other):
+        return self.__binaryOp(other,np.add)
+        
+    def __CheckCompatiblility(self,A,B):
+        if id(A.mesh) != id(B.mesh):
+            raise (Exception("The support of the fields are not the same"))
+        if id(A.rules) != id(B.rules):
+            raise (Exception("The rules of the fields are not the same"))
+            
     def __str__(self):
         res =  "IntegrationPointField\n"
         # ("+str(self.ncoomp)+")
         res += " name : "+ str(self.name) + "\n"
-        res += str({ name:data.shape for name,data in self.storage.items()} )  + "\n"
+        res += str({ name:data.shape for name,data in self.data.items()} )  + "\n"
         return res
 
-    def GetValueAtIP(self,name,el,ip):
-        return self.storage[name][el,ip]
-
-    def SetValueAtIP(self,name,el,ip,val):
-        self.storage[name][el,ip] = val
-
-    def IncrementValueAtIP(self,name,el,ip,val):
-        self.storage[name][el,ip] += val
-
-    def GetStorage(self,name):
-        return self.storage[name]
 
 def CheckIntegrity(GUI=False):
     from BasicTools.FE.IntegrationsRules import LagrangeP1
     from BasicTools.Containers.UnstructuredMeshTools import CreateCube
     mesh = CreateCube([10.,10.,10.],[-1.0,-1.0,-1.0],[2./10, 2./10,2./10])
 
-    eps = IntegrationPointField("Epsilon_0")
-    eps.ncoomp = 6
-    eps.Allocate(mesh,LagrangeP1)
+    eps = IntegrationPointField("Epsilon_0",mesh=mesh,rule=LagrangeP1)
+    eps.Allocate()
+    
     print(eps)
     return "ok"
 
