85ff97f0bc87afc001140fc2c9305fe44ab0b790
Felipe Bordeu <felipe.bordeu@safrangroup.com>
2020-05-12 09:38:55 +0200

    (FESpaces.py) Add Quadrangle_9 to the FESpaces

--- src/BasicTools/FE/Spaces/FESpaces.py
+++ src/BasicTools/FE/Spaces/FESpaces.py
@@ -32,6 +32,7 @@ ConstantSpaceGlobal[EN.Bar_2] = BarSpaces.Bar_P0_Global()
 ConstantSpaceGlobal[EN.Bar_3] = BarSpaces.Bar_P0_Global()
 ConstantSpaceGlobal[EN.Triangle_3] = TriSpaces.Tri_P0_Global()
 ConstantSpaceGlobal[EN.Quadrangle_4] = QuadSpaces.Quad_P0_Global()
+ConstantSpaceGlobal[EN.Quadrangle_9] = QuadSpaces.Quad_P0_Global()
 ConstantSpaceGlobal[EN.Tetrahedron_4] = TetSpaces.Tet_P0_Global()
 ConstantSpaceGlobal[EN.Hexaedron_8] = HexaSpaces.Hexa_P0_Global()
 
@@ -43,6 +44,7 @@ LagrangeSpaceP0[EN.Bar_3] = BarSpaces.Bar_P0_Lagrange()
 LagrangeSpaceP0[EN.Triangle_3] = TriSpaces.Tri_P0_Lagrange()
 LagrangeSpaceP0[EN.Tetrahedron_4] = TetSpaces.Tet_P0_Lagrange()
 LagrangeSpaceP0[EN.Quadrangle_4] = QuadSpaces.Quad_P0_Lagrange()
+LagrangeSpaceP0[EN.Quadrangle_9] = QuadSpaces.Quad_P0_Lagrange()
 LagrangeSpaceP0[EN.Hexaedron_8] = HexaSpaces.Hexa_P0_Lagrange()
 
 LagrangeSpaceP1 = {}
@@ -52,6 +54,7 @@ LagrangeSpaceP1[EN.Bar_3] = BarSpaces.Bar_P1_Lagrange()
 LagrangeSpaceP1[EN.Triangle_3] = TriSpaces.Tri_P1_Lagrange()
 LagrangeSpaceP1[EN.Tetrahedron_4] = TetSpaces.Tet_P1_Lagrange()
 LagrangeSpaceP1[EN.Quadrangle_4] = QuadSpaces.Quad_P1_Lagrange()
+LagrangeSpaceP1[EN.Quadrangle_9] = QuadSpaces.Quad_P1_Lagrange()
 LagrangeSpaceP1[EN.Hexaedron_8] = HexaSpaces.Hexa_P1_Lagrange()
 
 LagrangeSpaceP2 = {}
@@ -63,6 +66,7 @@ LagrangeSpaceP2[EN.Triangle_6] = TriSpaces.Tri_P2_Lagrange()
 LagrangeSpaceP2[EN.Tetrahedron_4] = TetSpaces.Tet_P2_Lagrange()
 LagrangeSpaceP2[EN.Tetrahedron_10] = TetSpaces.Tet_P2_Lagrange()
 LagrangeSpaceP2[EN.Quadrangle_4] = QuadSpaces.Quad_P2_Lagrange()
+LagrangeSpaceP2[EN.Quadrangle_9] = QuadSpaces.Quad_P2_Lagrange()
 LagrangeSpaceP2[EN.Hexaedron_8] = HexaSpaces.Hexa_P2_Lagrange()
 
 def CheckIntegrity(GUI=False):
