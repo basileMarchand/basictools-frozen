2f1710206a1ca78cc08e09e98db6e2d51aba9290
Julien Cortial <julien.cortial@safrangroup.com>
2020-10-23 10:22:44 +0200

    Simplify implementation

--- src/BasicTools/Helpers/Search.py
+++ src/BasicTools/Helpers/Search.py
@@ -33,52 +33,7 @@ def BinarySearch(ordered_list, item):
         the rank of the largest element smaller than item in the sorted data "list"
     """
 
-    first = 0
-    last = len(ordered_list) - 1
-
-    if item < ordered_list[first]:
-        return first
-
-    if item > ordered_list[last]:
-        return last
-
-    return index(ordered_list, find_gt(ordered_list, item)) - 1
-
-
-def index(a, x):
-    'Locate the leftmost value exactly equal to x'
-    i = bisect.bisect_left(a, x)
-    if i != len(a) and a[i] == x:
-        return i
-    raise ValueError
-
-def find_lt(a, x):
-    'Find rightmost value less than x'
-    i = bisect.bisect_left(a, x)
-    if i:
-        return a[i-1]
-    raise ValueError
-
-def find_le(a, x):
-    'Find rightmost value less than or equal to x'
-    i = bisect.bisect_right(a, x)
-    if i:
-        return a[i-1]
-    raise ValueError
-
-def find_gt(a, x):
-    'Find leftmost value greater than x'
-    i = bisect.bisect_right(a, x)
-    if i != len(a):
-        return a[i]
-    raise ValueError
-
-def find_ge(a, x):
-    'Find leftmost item greater than or equal to x'
-    i = bisect.bisect_left(a, x)
-    if i != len(a):
-        return a[i]
-    raise ValueError
+    return max(bisect.bisect_right(ordered_list, item) - 1, 0)
 
 
 def CheckIntegrity(GUI=False):
