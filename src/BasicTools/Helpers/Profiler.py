ff4442b8c9760cfa6f205cb0d2e6c53af6b1c066
Fabien Casenave <fabien.casenave@safrangroup.com>
2020-11-13 16:49:37 +0100

    (Profiler.py) improve outputs

--- src/BasicTools/Helpers/Profiler.py
+++ src/BasicTools/Helpers/Profiler.py
@@ -10,16 +10,18 @@ import pstats
 import io
 import numpy as np
 
-
 class Profiler():
 
-    def __init__(self):
+    def __init__(self, discardedPortion = 0.2, timerSignificantDigits = 3):
         self.pr = cProfile.Profile()
         self.s = io.StringIO()
         self.ps = None
-        self.discardedPortion = 0.2
+        self.discardedPortion = discardedPortion
+        self.timerSignificantDigits = timerSignificantDigits
         self.totTimes = None
         self.cumTimes = None
+        self.sumTotTime = None
+        self.sumCumTime = None
 
 
     def Start(self):
@@ -27,24 +29,21 @@ class Profiler():
 
 
     def Stop(self):
+
         self.pr.disable()
         self.ps = pstats.Stats(self.pr, stream=self.s).sort_stats('cumulative')
         self.ps.reverse_order()
         self.ps.print_stats()
 
 
-    def SetDiscardedPortion(self, discardedPortion):
-        self.discardedPortion = discardedPortion
-
-
-    def SortStats(self):
-
         lines = self.s.getvalue().split('\n')[5:-3]
-        functionNames = [l[46:][-30:] for l in lines]
+        functionNames = [l[46:][-60:] for l in lines]
         parsing = np.array([l.split()[:5] for l in lines])
 
         tottimes = np.array(parsing[:,1], dtype = float)
         cumtimes = np.array(parsing[:,3], dtype = float)
+        self.sumTotTime = np.sum(tottimes)
+        self.sumCumTime = np.sum(cumtimes)
 
         tottimes = tottimes / np.sum(tottimes)
         cumtimes = cumtimes / np.sum(cumtimes)
@@ -67,8 +66,8 @@ class Profiler():
         cumtimes = cumtimes[cumtimesArgSortInv]
         cumtimes = np.hstack((cumtimes, 1 - np.sum(cumtimes)))
 
-        functionNamestottime = [functionNames[i] for i in tottimesArgSortInv] + ["rest"]
-        functionNamescumtime = [functionNames[i] for i in cumtimesArgSortInv] + ["rest"]
+        functionNamestottime = [functionNames[i] for i in tottimesArgSortInv] + ["REMAINING"]
+        functionNamescumtime = [functionNames[i] for i in cumtimesArgSortInv] + ["REMAINING"]
 
 
         from collections import OrderedDict
@@ -81,31 +80,34 @@ class Profiler():
     def __str__(self):
 
         from BasicTools.Helpers.TextFormatHelper import TFormat
-        string = TFormat.InBlue("Profiler")+\
-        " discarding "+str(int(100.*self.discardedPortion))+"% of the smallest functions\n"
-        string += TFormat.InRed("total times:\n")+str(self.totTimes)+"\n"
-        string += TFormat.InRed("cumulated times:\n")+str(self.cumTimes)
+        string = TFormat.InBlue("Profiler")+" discarding "
+        string += str(int(100.*self.discardedPortion))+"% of the smallest functions\n"
+        string += TFormat.InRed("Total times:\n")
+
+        for part, name in self.totTimes.items():
+            string += TFormat.InGreen(TFormat.Center(name,width=70))+": "+str(round(100.*part,2))
+            string += "% ("+str(round(self.sumTotTime*part,self.timerSignificantDigits))+"s)\n"
+        string += TFormat.InRed("Cumulated  times:\n")
+        for part, name in self.cumTimes.items():
+            string += TFormat.InGreen(TFormat.Center(name,width=70))+": "+str(round(100.*part,2))
+            string += "% ("+str(round(self.sumCumTime*part,self.timerSignificantDigits))+"s)\n"
         return string
 
 
-        #return self.s.getvalue()[:-1] + "   ncalls  tottime  percall  cumtime  percall filename:lineno(function)"
+
 
 
 def CheckIntegrity(GUI=False):
 
     import time
 
-    p = Profiler()
+    p = Profiler(0.2)
+
     p.Start()
-    p.SetDiscardedPortion(0.2)
-
     time.sleep(0.002)
-    print("toto")
-
     p.Stop()
-    p.SortStats()
-    print(p)
 
+    print(p)
 
 
     return "ok"
