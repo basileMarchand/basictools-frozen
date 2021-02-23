14329358722020382874609fbd71bbe9dbbd2034
Felipe Bordeu <felipe.bordeu@safrangroup.com>
2021-02-23 14:34:38 +0100

    change to version 1.5

--- src/BasicTools/__init__.py
+++ src/BasicTools/__init__.py
@@ -17,10 +17,10 @@ _test = ["Actions",
 
 __name__ = "BasicTools"
 __copyright_holder__ = "Safran"
-__copyright_years__ = "2016-2020"
+__copyright_years__ = "2016-2021"
 __copyright__ = "{}, {}".format(__copyright_years__,__copyright_holder__)
 __license__ = "BSD 3-Clause License"
-__version__ = "1.4"
+__version__ = "1.5"
 
 
 def main():
