1c6d33103057a06ff7094a70a17ac14181914bd0
Felipe Bordeu <felipe.bordeu@safrangroup.com>
2021-04-14 23:13:16 +0200

    New cpp file for cpp_src/Containers/UnstructuredMeshTools.h

---
+++ cpp_src/Containers/UnstructuredMeshTools.h
@@ -0,0 +1,26 @@
+//
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+//
+#pragma once
+
+#include <Containers/UnstructuredMesh.h>
+
+MatrixDDD GetElementsCenters(MapMatrixDDD& nodes, const ElementsContainer& elements){
+
+     auto& connectivity = elements.GetConnectivityMatrix();
+     MatrixDDD res(elements.GetNumberOfElements(), nodes.cols());
+     res.fill(0.0);
+     for(Eigen::Index i=0; i <nodes.cols(); ++i){
+         MatrixDDD a = indexingi(nodes,connectivity,i);
+
+         MatrixDD1 b= a.rowwise().sum().eval().col(0);
+
+         //MatrixDD1 c = (b.col(0)+ res.col(i)).eval();
+         res.col(i) = (b.col(0)+ res.col(i)).eval();
+         //res.col(i) += indexingi(nodes,connectivity,i).rowwise().sum();
+     }
+     res /= connectivity.cols();
+     return res;
+
+};
