4fa6b05da666f8b3e578c425410c53901cfa485b
Felipe Bordeu <felipe.bordeu@safrangroup.com>
2021-04-19 17:21:10 +0200

    (ElementNames.py/.h) Add namespace and move code to the .cpp

--- cpp_src/Containers/ElementNames.h
+++ cpp_src/Containers/ElementNames.h
@@ -8,6 +8,9 @@
 #include <string>
 #include <LinAlg/EigenTypes.h>
 
+namespace BasicTools
+{
+    
 class GeoSupport {
 public:
     std::string name;
@@ -18,15 +21,15 @@ public:
     }
 };
 
-GeoSupport GeoNA("NA",-1);
-GeoSupport GeoPoint("point",0);
-GeoSupport GeoBar("bar"  ,1);
-GeoSupport GeoTri("tri"  ,2);
-GeoSupport GeoQuad("quad" ,2);
-GeoSupport GeoTet("tet"  ,3);
-GeoSupport GeoPyr("pyr"  ,3);
-GeoSupport GeoWed("wed"  ,3);
-GeoSupport GeoHex("hex"  ,3 );
+extern GeoSupport GeoNA;
+extern GeoSupport GeoPoint;
+extern GeoSupport GeoBar;
+extern GeoSupport GeoTri;
+extern GeoSupport GeoQuad;
+extern GeoSupport GeoTet;
+extern GeoSupport GeoPyr;
+extern GeoSupport GeoWed;
+extern GeoSupport GeoHex;
 
 class ElementInfo{
 public:
@@ -41,23 +44,23 @@ public:
     int dimension() const { return this->geoSupport.dimensionality; }
 };
 
-const std::string Point_1 = "point1";
-const std::string Bar_2 = "bar2";
-const std::string Bar_3 = "bar3";
-const std::string Triangle_6 = "tri6";
-const std::string Quadrangle_8 = "quad8";
-const std::string Quadrangle_9 = "quad9";
-const std::string Tetrahedron_4 = "tet4";
-const std::string Pyramid_5 = "pyr5";
-const std::string Wedge_6 = "wed6";
-const std::string Hexaedron_8 = "hex8";
-const std::string Tetrahedron_10 = "tet10";
-const std::string Pyramid_13 = "pyr13";
-const std::string Wedge_15 = "wed15";
-const std::string Wedge_18 = "wed18";
-const std::string Hexaedron_20 = "hex20";
-const std::string Hexaedron_27 = "hex27";
+extern const std::string Point_1;
+extern const std::string Bar_2;
+extern const std::string Bar_3;
+extern const std::string Triangle_6;
+extern const std::string Quadrangle_8;
+extern const std::string Quadrangle_9;
+extern const std::string Tetrahedron_4;
+extern const std::string Pyramid_5;
+extern const std::string Wedge_6;
+extern const std::string Hexaedron_8;
+extern const std::string Tetrahedron_10;
+extern const std::string Pyramid_13;
+extern const std::string Wedge_15;
+extern const std::string Wedge_18;
+extern const std::string Hexaedron_20;
+extern const std::string Hexaedron_27;
 
-std::map<std::string,ElementInfo> InitElementNames() ;
+extern std::map<std::string,ElementInfo> ElementNames;
 
-std::map<std::string,ElementInfo> ElementNames = InitElementNames();
+} // namespace BasicTools
--- src/BasicTools/Containers/ElementNames.py
+++ src/BasicTools/Containers/ElementNames.py
@@ -255,9 +255,42 @@ faces[Hexaedron_27] = [(Quadrangle_9,[3, 0, 4, 7,11,16,15,19,20]),
 def GeneratertElementNamesCpp(filename = "cpp_src/Containers/ElementNames.cpp"):
     try:
         with open(filename,"w") as file:
-            file.write("""
+            file.write("""//
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+//
+
 #include <Containers/ElementNames.h>
 #include <map>
+
+namespace BasicTools{
+
+GeoSupport GeoNA("NA",-1);
+GeoSupport GeoPoint("point",0);
+GeoSupport GeoBar("bar"  ,1);
+GeoSupport GeoTri("tri"  ,2);
+GeoSupport GeoQuad("quad" ,2);
+GeoSupport GeoTet("tet"  ,3);
+GeoSupport GeoPyr("pyr"  ,3);
+GeoSupport GeoWed("wed"  ,3);
+GeoSupport GeoHex("hex"  ,3 );
+
+const std::string Point_1 = "point1";
+const std::string Bar_2 = "bar2";
+const std::string Bar_3 = "bar3";
+const std::string Triangle_6 = "tri6";
+const std::string Quadrangle_8 = "quad8";
+const std::string Quadrangle_9 = "quad9";
+const std::string Tetrahedron_4 = "tet4";
+const std::string Pyramid_5 = "pyr5";
+const std::string Wedge_6 = "wed6";
+const std::string Hexaedron_8 = "hex8";
+const std::string Tetrahedron_10 = "tet10";
+const std::string Pyramid_13 = "pyr13";
+const std::string Wedge_15 = "wed15";
+const std::string Wedge_18 = "wed18";
+const std::string Hexaedron_20 = "hex20";
+const std::string Hexaedron_27 = "hex27";
 
 std::map<std::string,ElementInfo> InitElementNames() {
     std::map<std::string,ElementInfo> ElementNames;
@@ -293,7 +326,14 @@ std::map<std::string,ElementInfo> InitElementNames() {
                    file.write("    }\n")
             file.write("""
     return ElementNames;
-};\n""")
+};
+
+std::map<std::string,ElementInfo> InitElementNames() ;
+std::map<std::string,ElementInfo> ElementNames = InitElementNames();
+
+}; // namespace BasicTools
+
+\n""")
 
     except Exception:
         print('Unable to create file '+ filename)
