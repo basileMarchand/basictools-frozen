56a9104633e4a0af95ad3bec39d32929dcb286ce
Felipe Bordeu <felipe.bordeu@safrangroup.com>
2021-04-20 10:46:40 +0200

    (EigenTools.h/.cpp) Separation of code

---
+++ cpp_src/LinAlg/EigenTools.cpp
@@ -0,0 +1,80 @@
+//
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+//
+
+#include <cassert>
+#include <LinAlg/EigenTools.h>
+
+namespace BasicTools
+{
+    
+MatrixID1 NonZero(const MatrixBD1& input){
+    const INT_TYPE& rows = input.array().count();
+    MatrixID1 res(rows,1);
+    INT_TYPE cpt=0;
+    for(INT_TYPE i=0; i < input.cols();++i  ){
+        if (input(i,0)){
+            res(cpt++,0) = i;
+        }
+    };
+    return res;
+};
+
+
+MatrixID1 Intersect1D(const MatrixID1& A, const MatrixID1& B){
+    MatrixID1 res(min(A.rows(),B.rows()),1);
+    INT_TYPE cptA= 0;
+    INT_TYPE cptB= 0;
+    INT_TYPE cpt= 0;
+    while(cptA < A.rows() or cptB < B.rows() ){
+       if( A(cptA,0) < B(cptB,0)){
+           ++cptA;
+       } else if( A(cptA,0) > B(cptB,0)){
+           ++cptB;
+       } else{
+           res(cpt,0) = B(cptB,0);
+           ++cpt;
+           ++cptA;
+           ++cptB;
+       }
+    }
+    res.conservativeResize(cpt,1);
+    return res;
+};
+//
+MatrixID1 Union1D(const MatrixID1& A, const MatrixID1& B){
+    MatrixID1 res(A.rows()+B.rows(),1);
+    INT_TYPE cptA= 0;
+    INT_TYPE cptB= 0;
+    INT_TYPE cpt= 0;
+    while(cptA < A.rows() or cptB < B.rows() ){
+       if( A(cptA,0) < B(cptB,0)){
+           res(cpt,0) = A(cptA,0);
+           ++cpt;
+           ++cptA;
+       } else if ( A(cptA,0) > B(cptB,0)){
+           res(cpt,0) = B(cptB,0);
+           ++cpt;
+           ++cptB;
+       } else{
+           res(cpt,0) = B(cptB,0);
+           ++cpt;
+           ++cptA;
+           ++cptB;
+       }
+    }
+    for(;cptA < A.rows(); ++ cptA ){
+       res(cpt,0) = A(cptA,0);
+       ++cpt;
+    }
+    for(;cptB < B.rows(); ++ cptB ){
+       res(cpt,0) = B(cptB,0);
+       ++cpt;
+    }
+    res.conservativeResize(cpt,1);
+    return res;
+}
+//  from http://eigen.tuxfamily.org/dox/TopicCustomizing_NullaryExpr.html
+
+} // namespace BasicTools
--- cpp_src/LinAlg/EigenTools.h
+++ cpp_src/LinAlg/EigenTools.h
@@ -11,79 +11,24 @@
 #define max(a,b) ((a)>=(b)?(a):(b))
 #define min(a,b) ((a)<=(b)?(a):(b))
 
+namespace BasicTools
+{
+    
+MatrixID1 NonZero(const MatrixBD1& input);
 
-template<typename T>
-MatrixID1 where(const T& input){
-    if(input.cols() != 1){
-        throw  "cols must be one";
-    }
-    const INT_TYPE& rows = input.array().count();
-    MatrixID1 res(rows,1);
-    INT_TYPE cpt=0;
-    for(INT_TYPE i=0; i < input.cols();++i  ){
-        if (input(i,0)){
-            res(cpt,0) = i;
-            ++cpt;
-        }
-    };
-    return res;
-};
+MatrixID1 Intersect1D(const MatrixID1& A, const MatrixID1& B);
 
-MatrixID1 Intersect1D(const MatrixID1& A, const MatrixID1& B){
-    MatrixID1 res(min(A.rows(),B.rows()),1);
-    INT_TYPE cptA= 0;
-    INT_TYPE cptB= 0;
-    INT_TYPE cpt= 0;
-    while(cptA < A.rows() or cptB < B.rows() ){
-       if( A(cptA,0) < B(cptB,0)){
-           ++cptA;
-       } else if( A(cptA,0) > B(cptB,0)){
-           ++cptB;
-       } else{
-           res(cpt,0) = B(cptB,0);
-           ++cpt;
-           ++cptA;
-           ++cptB;
-       }
-    }
-    res.conservativeResize(cpt,1);
-    return res;
-};
+MatrixID1 Union1D(const MatrixID1& A, const MatrixID1& B);
+
 //
-MatrixID1 Union1D(const MatrixID1& A, const MatrixID1& B){
-    MatrixID1 res(A.rows()+B.rows(),1);
-    INT_TYPE cptA= 0;
-    INT_TYPE cptB= 0;
-    INT_TYPE cpt= 0;
-    while(cptA < A.rows() or cptB < B.rows() ){
-       if( A(cptA,0) < B(cptB,0)){
-           res(cpt,0) = A(cptA,0);
-           ++cpt;
-           ++cptA;
-       } else if ( A(cptA,0) > B(cptB,0)){
-           res(cpt,0) = B(cptB,0);
-           ++cpt;
-           ++cptB;
-       } else{
-           res(cpt,0) = B(cptB,0);
-           ++cpt;
-           ++cptA;
-           ++cptB;
-       }
-    }
-    for(;cptA < A.rows(); ++ cptA ){
-       res(cpt,0) = A(cptA,0);
-       ++cpt;
-    }
-    for(;cptB < B.rows(); ++ cptB ){
-       res(cpt,0) = B(cptB,0);
-       ++cpt;
-    }
-    res.conservativeResize(cpt,1);
-    return res;
-}
 //  from http://eigen.tuxfamily.org/dox/TopicCustomizing_NullaryExpr.html
-
+// usage:
+//  Eigen::MatrixXi A = Eigen::MatrixXi::Random(4,4);
+//  Array3i ri(1,2,1);
+//  ArrayXi ci(6); ci << 3,2,1,0,0,2;
+//  Eigen::MatrixXi B = indexingij(A, ri, ci); // numpy like B = A[ri,:][:,ci]
+//  Eigen::MatrixXi B = indexingi(A, ri, 0); // numpy like B = A[ri,0]
+//  Eigen::MatrixXi B = indexingj(A, 1, ci); // numpy like B = A[1,ci]
 //***********************  Indexing A(vec,vec)********************
 template<class ArgType, class RowIndexType, class ColIndexType>
 class indexing_functorij {
@@ -216,11 +161,6 @@ indexingm(const Eigen::MatrixBase<ArgType>& arg, const RowColIndexType& rowcol_i
   return MatrixType::NullaryExpr(rowcol_indices.rows(), rowcol_indices.cols(), Func(arg.derived(), rowcol_indices));
 }
 
-
-// usage:
-//  Eigen::MatrixXi A = Eigen::MatrixXi::Random(4,4);
-//  Array3i ri(1,2,1);
-//  ArrayXi ci(6); ci << 3,2,1,0,0,2;
-//  Eigen::MatrixXi B = indexing(A, ri, ci); // numpy like B = A[ri,:][:,ci]
 //***********************   ********************
 
+} // namespace BasicTools
