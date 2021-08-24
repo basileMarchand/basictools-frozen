a8d8d890e9014832329a94999d762270641ce749
Felipe Bordeu <felipe.bordeu@safrangroup.com>
2021-08-24 10:59:01 +0200

    (UniversalReader.Py) Separation of the InitAllReaders

--- src/BasicTools/IO/UniversalReader.py
+++ src/BasicTools/IO/UniversalReader.py
@@ -7,9 +7,11 @@
 
 import BasicTools.Containers.ElementNames as ElementNames
 
+def InitAllReaders():
+    from BasicTools.IO.IOFactory import InitAllReaders as IAR
+    IAR()
+
 def ReadMesh(filename,out=None,timeToRead=-1):# pragma: no cover
-    from BasicTools.IO.IOFactory import InitAllReaders
-    InitAllReaders()
     import os.path
 
     dirname = os.path.dirname(filename)
@@ -30,55 +32,6 @@ def ReadMesh(filename,out=None,timeToRead=-1):# pragma: no cover
             print(timeToRead)
 
     return reader.Read()
-
-    if extention ==  ".asc":
-        import BasicTools.IO.AscReader as AscReader
-        return AscReader.ReadAsc(filename)
-    elif extention ==  ".geof":
-        import BasicTools.IO.GeofReader as GeofReader
-        return GeofReader.ReadGeof(filename)
-    elif extention ==  ".msh":
-        import BasicTools.IO.GmshReader as GmshReader
-        return GmshReader.ReadGmsh(filename,out=out)
-    elif extention ==  ".inp":
-        import BasicTools.IO.InpReader as ImpReader
-        return ImpReader.ReadInp(filename)
-    elif extention ==  ".mesh":
-        import BasicTools.IO.MeshReader as ReadMesh
-        return ReadMesh.ReadMesh(fileName=filename)
-    elif extention ==  ".meshb":
-        import BasicTools.IO.MeshReader as MeshReader
-        return MeshReader.ReadMesh(fileName=filename)
-    elif extention ==  ".gcode":
-        import BasicTools.IO.GReader as GReader
-        return GReader.ReadGCode(fileName=filename)
-    elif extention ==  ".fem":
-        import BasicTools.IO.FemReader as FemReader
-        return FemReader.ReadFem(fileName=filename)
-    elif extention ==  ".solb" or extention ==  ".sol":
-        import BasicTools.IO.MeshReader as MeshReader
-        return MeshReader.ReadSol(fileName=filename)
-    elif extention ==  ".ut" or extention ==  ".utp":
-        from BasicTools.IO.UtReader import ReadMeshAndUt
-        return ReadMeshAndUt(fileName=filename)
-    elif extention == ".stl":
-        from BasicTools.IO.StlReader import ReadStl
-        return ReadStl(fileName=filename)
-    else:
-        from BasicTools.IO.IOFactory import CreateReader
-        CreateReader(extention)
-        raise Exception ("Unkown file extention : " + str(extention))
-
-
-## to use this function add this lines to the
-## programmmable source in paraview, and change the output type to UnstructuredGrid
-##
-#
-#UniversalReader.ReadMeshAnPopulateVtkObject(filename,self.GetOutput())
-#from BasicTools.IO.UniversalReader import ReadMeshAndPopulateVtkObject as ReadMeshAndPopulateVtkObject
-#filename = "here you put your filename"
-#ReadMeshAndPopulateVtkObject(filename,self.GetOutput(),TagsAsFields=True/False )
-
 
 def ReadMeshAndPopulateVtkObject(filename, vtkobject= None,TagsAsFields=False):# pragma: no cover
     mesh = ReadMesh(filename)
