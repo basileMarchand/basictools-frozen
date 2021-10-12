80934a6763f467947d26c2b71257395e4913e71d
Felipe Bordeu <felipe.bordeu@safrangroup.com>
2021-10-12 13:58:03 +0200

    Integration rule h

---
+++ cpp_src/FE/IntegrationRule.h
@@ -0,0 +1,29 @@
+//
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+//
+#pragma once
+#include <memory>
+
+#include <LinAlg/EigenTypes.h>
+
+namespace BasicTools { 
+
+
+class IntegrationRule {
+public:
+    MatrixDDD p;
+    MatrixDDD w;
+};
+
+class SpaceIntegrationRule {
+public: 
+    std::map<std::string,IntegrationRule> storage;
+    const IntegrationRule& GetIR(const std::string& key ) const {
+        return this->storage.at(key);
+    }
+};
+
+
+};// BasicTools namespace
+
