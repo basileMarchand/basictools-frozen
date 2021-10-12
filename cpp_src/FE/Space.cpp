6281ae598a5349f4afe71b61c40d319895e2ae1f
Felipe Bordeu <felipe.bordeu@safrangroup.com>
2021-10-12 13:38:18 +0200

    Space.cpp with pointer to store shape functions functions

--- cpp_src/FE/Space.cpp
+++ cpp_src/FE/Space.cpp
@@ -20,6 +20,23 @@ const DofAttachment& ElementSpace::GetDofAttachment(const int& dofnumber) const 
 void ElementSpace::AppendDofAttachement(const char& entity, const int& entityNumber, const int& extraKey){
     storage.push_back( DofAttachment(entity,entityNumber, extraKey) ) ;
 }
+
+const MatrixDDD ElementSpace::GetValOfShapeFunctionsAt(const MatrixDDD& phixieta  ) const {
+    return this->GetValOfShapeFunctionsAt(phixieta.coeff(0,0), phixieta.coeff(1,0),phixieta.coeff(2,0));
+};
+
+const MatrixDDD ElementSpace::GetValOfShapeFunctionsAt(const double& phi, const double&  xi, const double&  eta  ) const {
+    return this->SFV(phi, xi, eta);
+};
+
+const MatrixDDD ElementSpace::GetValOfShapeFunctionsDerAt(const MatrixDDD& phixieta  ) const {
+    return this->GetValOfShapeFunctionsDerAt(phixieta.coeff(0,0), phixieta.coeff(1,0),phixieta.coeff(2,0));
+};
+
+const MatrixDDD ElementSpace::GetValOfShapeFunctionsDerAt(const double& phi, const double&  xi, const double&  eta  ) const {
+    return this->SFDV(phi, xi, eta);
+};
+
 // ***************  Space ******************
 CBasicIndexType Space::GetNumberOfShapeFunctionsFor(const std::string& elemtype){
     return this->storage[elemtype].GetNumberOfShapeFunctions();
@@ -29,7 +46,7 @@ void Space::AddDofTo(const std::string& elemtype, const char& entity, const int&
     this->storage[elemtype].AppendDofAttachement(entity, entityNumber, extraKey);
 }
 //
-ElementSpace& Space::GetSpaceFor(const std::string& elemtype){
+const ElementSpace& Space::GetSpaceFor(const std::string& elemtype)  {
     return this->storage[elemtype];
 }
 //
@@ -40,5 +57,27 @@ std::string ToStr(){
     std::string res = "Space \n";
     return res;
 }
+// ******************* space *************************
+std::map<std::string, SpaceAtIP> EvaluateSpaceAt(const Space& sp, const SpaceIntegrationRule&  sir){
+    std::map<std::string, SpaceAtIP> res;
+    for(const  auto& item: sp.storage ){
+        std::string key = item.first;
+        res[key] =  EvaluateSpaceAt( item.second, sir.GetIR(key));
+    }
+    return res;
+};
+
+SpaceAtIP EvaluateSpaceAt(const ElementSpace& es , const IntegrationRule& ir ){
+    SpaceAtIP res;
+    const int nbp = ir.GetNumberOfPoints();
+    for(int i=0; i<nbp; ++i){
+        res.SFV = es.GetValOfShapeFunctionsAt(ir.p.row(i));
+        res.SFDV = es.GetValOfShapeFunctionsDerAt(ir.p.row(i));
+    }
+    return res;
+};
 
 } // namespace BasicTools
+
+
+
--- cpp_src/FE/Space.h
+++ cpp_src/FE/Space.h
@@ -10,6 +10,7 @@
 #include <memory>
 
 #include <LinAlg/EigenTypes.h>
+#include <FE/IntegrationRule.h>
 
 namespace BasicTools
 {
@@ -24,19 +25,39 @@ struct DofAttachment {
 struct ElementSpace{
     std::vector<DofAttachment > storage;
     ElementSpace(){};
+
+    MatrixDDD (*SFV)(const double&,const double&,const double& );
+    MatrixDDD (*SFDV)(const double&,const double&,const double& );
     CBasicIndexType GetNumberOfShapeFunctions();
     const DofAttachment& GetDofAttachment(const int& dofnumber) const ;
     void AppendDofAttachement(const char& entity, const int& entityNumber, const int& extraKey);
+    const MatrixDDD GetValOfShapeFunctionsAt(const double& phi, const double&  xi, const double&  eta  ) const ;
+    const MatrixDDD GetValOfShapeFunctionsAt(const MatrixDDD& phixieta) const ;
+    const MatrixDDD GetValOfShapeFunctionsDerAt(const double& phi, const double&  xi, const double&  eta) const  ;
+    const MatrixDDD GetValOfShapeFunctionsDerAt(const MatrixDDD& phixieta) const ;
 };
 
 class Space{
+public:
     std::map<std::string, ElementSpace > storage;
 public:
     CBasicIndexType GetNumberOfShapeFunctionsFor(const std::string& elemtype);
     void AddDofTo(const std::string& elemtype, const char& entity, const int& entityNumber, const int& extraKey);
-    ElementSpace& GetSpaceFor(const std::string& elemtype);
+    const ElementSpace& GetSpaceFor(const std::string& elemtype);
     void Print();
     std::string ToStr();
 };
+
+const Space& GetSpaceFor(const std::string& spacename);
+
+class SpaceAtIP{
+public:
+    MatrixDDD SFV;
+    MatrixDDD SFDV;
+};
+
+std::map<std::string, SpaceAtIP> EvaluateSpaceAt(const Space&, const SpaceIntegrationRule& );
+
+SpaceAtIP EvaluateSpaceAt(const ElementSpace&, const IntegrationRule& );
 
 } // namespace BasicTools
