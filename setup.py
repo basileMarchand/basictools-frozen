04bcec92e809902f42be193fe66c661a350b74e0
BORDEU Felipe <felipe.bordeu@safrangroup.com>
2021-12-08 09:38:35 +0100

    (setup.py) copatible with conda package generations

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@
 import os, sys
 from setuptools.command.build_ext import build_ext
 from setuptools.command.build_clib import build_clib
-from setuptools import setup, Extension
+from setuptools import setup, Extension, Command
+from distutils.command.build import build
 import configparser
 
 # Compilation options
@@ -34,7 +35,6 @@ cpp_src = ("LinAlg/BasicOperations.cpp",
            "FE/Space.cpp",
            )
 
-
 # Cython modules
 cython_src = (
     "Linalg/NativeEigenSolver.pyx",
@@ -49,33 +49,33 @@ cpp_generators = ["cpp_generators/IntegrationRuleGenerator.py",
                   "cpp_generators/ElementNameGenerator.py",
                   "cpp_generators/SpaceGenerator.py",
                   ]
+
 def GetBasicToolsIncludeDirs():
-    try:
-        import numpy
-        include_dirs =[numpy.get_include(),"cpp_src" ,"."]
+    include_dirs = []
+    if "CONDA_PREFIX" in os.environ:
+        conda_prefix = os.environ["CONDA_PREFIX"]
+        include_dirs.append(os.path.join(conda_prefix, "include"))
+        include_dirs.append(os.path.join(conda_prefix, "include", "eigen3"))
+        include_dirs.append(os.path.join(conda_prefix, "Library", "include"))
+        include_dirs.append(os.path.join(conda_prefix, "Library", "include", "eigen3"))
 
+    if "PREFIX" in os.environ:
+        conda_prefix = os.environ["PREFIX"]
+        include_dirs.append(os.path.join(conda_prefix, "include"))
+        include_dirs.append(os.path.join(conda_prefix, "include", "eigen3"))
+        include_dirs.append(os.path.join(conda_prefix, "Library", "include"))
+        include_dirs.append(os.path.join(conda_prefix, "Library", "include", "eigen3"))
 
-        import eigency
-        include_dirs.extend(eigency.get_includes(include_eigen=useEigencyEigen) )
-        if not useEigencyEigen:
-            if "EIGEN_INC" in os.environ:
-                include_dirs.append(os.environ.get('EIGEN_INC'))
-        if "CONDA_PREFIX" in os.environ:
-            conda_prefix = os.environ["CONDA_PREFIX"]
-            include_dirs.append(os.path.join(conda_prefix, "include"))
-            include_dirs.append(os.path.join(conda_prefix, "include", "eigen3"))
-            include_dirs.append(os.path.join(conda_prefix, "Library", "include"))
-            include_dirs.append(os.path.join(conda_prefix, "Library", "include", "eigen3"))
+    import numpy
+    include_dirs.extend([numpy.get_include(),"cpp_src" ,"."])
 
-        if "PREFIX" in os.environ:
-            conda_prefix = os.environ["PREFIX"]
-            include_dirs.append(os.path.join(conda_prefix, "include"))
-            include_dirs.append(os.path.join(conda_prefix, "include", "eigen3"))
-            include_dirs.append(os.path.join(conda_prefix, "Library", "include"))
-            include_dirs.append(os.path.join(conda_prefix, "Library", "include", "eigen3"))
-        return include_dirs
-    except :
-        return include_dirs
+    import eigency
+    include_dirs.extend(eigency.get_includes(include_eigen=useEigencyEigen) )
+    if not useEigencyEigen:
+        if "EIGEN_INC" in os.environ:
+            include_dirs.append(os.environ.get('EIGEN_INC'))
+    return include_dirs
+
 class add_path():
     def __init__(self, path):
         self.path = path
@@ -90,55 +90,32 @@ class add_path():
             pass
 
 try:
-    from Cython.Build import cythonize
-    from Cython.Compiler import Options
-    import eigency
     with add_path('./src/'):
         for generator in cpp_generators:
             code = compile(open(generator).read(),generator,"exec")
             res = {}
             exec(code,res)
             generated_file  = res["GetGeneratedFiles"]("")
-            print("generation of files : \n"+ "\n".join("cpp_src/"+str(gf)for gf in generated_file))
-            res["Generate"]("cpp_src/")
             cpp_src = generated_file + cpp_src
 
-    Options.fast_fail = True
-    Options.embed = True
-
     modules = []
+    cpp_src_with_path = [os.path.join("cpp_src", src) for src in cpp_src]
 
-    cythonextension = []
-
-    basictools_cpp_src_path = os.path.join("cpp_src")
-    cpp_src_with_path = [os.path.join(basictools_cpp_src_path, src) for src in cpp_src]
-
-    define_macros = []
-    libraries = ["libCppBasicTools"]
-    if enable_MKL or True:
-        define_macros.append(("MKL_DIRECT_CALL",""))
-        define_macros.append(("EIGEN_USE_MKL_VML",""))
 
     if debug:
         cpp_src_with_path = sorted(cpp_src_with_path,key=os.path.getmtime,reverse=True )
 
     ext_libraries = [['libCppBasicTools', {
                'sources': cpp_src_with_path,
-               'include_dirs':GetBasicToolsIncludeDirs(),
-               'macros': define_macros,
                }
     ]]
 
-    basictools_src_path = os.path.join("src", "BasicTools")
-    cython_src_with_path  = [os.path.join(basictools_src_path, src) for src in cython_src]
+    cython_src_with_path  = [os.path.join("src", "BasicTools",src) for src in cython_src]
 
     for n,m in zip(cython_src,cython_src_with_path):
-        cythonextension.append(Extension("BasicTools."+n.split(".pyx")[0].replace("/","."), [m],
-        libraries=libraries,
-        include_dirs=GetBasicToolsIncludeDirs(),
-        define_macros=define_macros,
+        modules.append(Extension("BasicTools."+n.split(".pyx")[0].replace("/","."), [m],
+        libraries=["libCppBasicTools"],
         language="c++"  ))
-    modules.extend(cythonextension)
 
 except ImportError as e:
     print(f"Compilation disabled since {e.name} package is missing")
@@ -148,8 +125,60 @@ except Exception as e:
     print("Error during generation of cpp sources ")
     print(e)
 
+class GenerateCommand(Command):
+    description = "custom generate command that generate the c++ sources from python "
+    user_options = []
+    def initialize_options(self):
+        self.cwd = None
+    def finalize_options(self):
+        self.cwd = os.getcwd()
+    def run(self):
+        assert os.getcwd() == self.cwd, 'Must be in package root: %s' % self.cwd
+        with add_path('./src/'):
+            for generator in cpp_generators:
+                code = compile(open(generator).read(),generator,"exec")
+                res = {}
+                exec(code,res)
+                generated_file  = res["GetGeneratedFiles"]("")
+                print("generation of files : \n"+ "\n".join("cpp_src/"+str(gf)for gf in generated_file))
+                res["Generate"]("cpp_src/")
+                #cpp_src = generated_file + cpp_src
+
+class my_build(build):
+    def run(self):
+        self.run_command("generate")
+        build.run(self)
+
+class my_build_ext(build_ext):
+    def build_extensions(self):
+        for ext in self.extensions:
+            ext.include_dirs.extend(GetBasicToolsIncludeDirs())
+        build_ext.build_extensions(self)
+
+class my_build_clib(build_clib):
+
+    def build_libraries(self,libraries):
+        define_macros = []
+        if enable_MKL or True:
+            define_macros.append(("MKL_DIRECT_CALL",None))
+            define_macros.append(("EIGEN_USE_MKL_VML",None))
+
+        include_dirs_BasicTools = GetBasicToolsIncludeDirs()
+        for (lib_name, build_info) in libraries:
+            include_dirs = build_info.get("include_dirs",[])
+            include_dirs.extend(include_dirs_BasicTools)
+            build_info["include_dirs"] = include_dirs
+
+            macros = build_info.get('macros',[])
+            macros.extend(define_macros)
+            build_info["macros"] = macros
+
+        build_clib.build_libraries(self,libraries)
+
 if __name__ == '__main__':
     setup(
         ext_modules=modules,
         libraries=ext_libraries,
+        cmdclass={'build': build,'build_ext':my_build_ext,'build_clib': my_build_clib,'generate': GenerateCommand},
+        data_files=[("ParaViewPlugins",["extras/BasicToolsParaViewBridge.py"])]
 )
