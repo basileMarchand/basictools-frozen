5943acf01cd750a2ba2b9ae8e733b65821e1ab0f
Felipe Bordeu <felipe.bordeu@safrangroup.com>
2022-05-04 11:15:40 +0200

    (which.py) cleaning and doc

--- src/BasicTools/Helpers/which.py
+++ src/BasicTools/Helpers/which.py
@@ -4,18 +4,35 @@
 # file 'LICENSE.txt', which is part of this source code package.
 #
 
-"""Class to check if a executable is in the current path
+from typing import Union
 
-insperated from:
-    https://stackoverflow.com/questions/377017/test-if-executable-exists-in-python
-"""
-def which(program):
+def which(program: str) -> Union[str,None]:
+    """Check if an executable is reachable
+    and return the real "executable" (with the .exe or .bat )
+
+    inspiration from:
+        https://stackoverflow.com/questions/377017/test-if-executable-exists-in-python
+
+    Parameters
+    ----------
+    program : str
+        the name of the programe to find, in windows the user can omit the .exe or the .bat
+        extentions
+
+    Returns
+    -------
+    Union[str,None]
+        return the string of the real executable or None if the executable is not found
+    """
+
     import os
     def is_exe(fpath):
         return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
 
     fpath, fname = os.path.split(program)
+
     if fpath:
+        # we have a path so we search for the exact filename
         if is_exe(program):
             return program
     else:
@@ -24,21 +41,30 @@ def which(program):
             exe_file = os.path.join(path, program)
             if is_exe(exe_file):
                 return exe_file
-            if os.name == "nt":# Windows
+            if os.name == "nt":# Windows # pragma: no cover
                 try:
                     from win32api import FindExecutable, GetLongPathName
+                except:
+                    pass
+                else:
                     _, executable = FindExecutable(program)
                     if os.path.isfile(executable):
                         return executable
-                except:
-                    pass
 
     return None
 
 def CheckIntegrity():
+    import os
+
     print(which("ls"))
     print(which("dir"))
+    print(which("lls"))
+    if os.name == "nt": # pragma: no cover
+        pass
+    else:
+        print(which("/usr/bin/ls"))
+
     return "OK"
 
-if __name__ == '__main__':
-    print((CheckIntegrity()))# pragma: no cover
+if __name__ == '__main__': # pragma: no cover
+    print((CheckIntegrity()))
