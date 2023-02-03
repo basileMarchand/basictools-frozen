61e9d9566476e4f6b1b92d9686720acecb2e7040
Felipe Bordeu <felipe.bordeu@safrangroup.com>
2023-02-03 17:51:22 +0100

    (ElementNames.h) Better definition of elements names

--- cpp_src/Containers/ElementNames.h
+++ cpp_src/Containers/ElementNames.h
@@ -45,24 +45,25 @@ public:
     int dimension() const { return this->geoSupport.dimensionality; }
 };
 
-extern const std::string Point_1;
-extern const std::string Bar_2;
-extern const std::string Bar_3;
-extern const std::string Triangle_3;
-extern const std::string Triangle_6;
-extern const std::string Quadrangle_4;
-extern const std::string Quadrangle_8;
-extern const std::string Quadrangle_9;
-extern const std::string Tetrahedron_4;
-extern const std::string Pyramid_5;
-extern const std::string Wedge_6;
-extern const std::string Hexaedron_8;
-extern const std::string Tetrahedron_10;
-extern const std::string Pyramid_13;
-extern const std::string Wedge_15;
-extern const std::string Wedge_18;
-extern const std::string Hexaedron_20;
-extern const std::string Hexaedron_27;
+inline const std::string Point_1 = "point1";
+inline const std::string Bar_2 = "bar2";
+inline const std::string Bar_3 = "bar3";
+inline const std::string Triangle_3 = "tri3";
+inline const std::string Triangle_6 = "tri6";
+inline const std::string Quadrangle_4 = "quad4";
+inline const std::string Quadrangle_8 = "quad8";
+inline const std::string Quadrangle_9 = "quad9";
+inline const std::string Tetrahedron_4 = "tet4";
+inline const std::string Pyramid_5 = "pyr5";
+inline const std::string Wedge_6 = "wed6";
+inline const std::string Hexaedron_8 = "hex8";
+inline const std::string Tetrahedron_10 = "tet10";
+inline const std::string Pyramid_13 = "pyr13";
+inline const std::string Wedge_15 = "wed15";
+inline const std::string Wedge_18 = "wed18";
+inline const std::string Hexaedron_20 = "hex20";
+inline const std::string Hexaedron_27 = "hex27";
+
 
 extern std::map<std::string,ElementInfo> ElementNames;
 
