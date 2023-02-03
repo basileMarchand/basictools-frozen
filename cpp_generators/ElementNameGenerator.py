f9e4be446f72d969aa317ad339d11962d0dd5231
Felipe Bordeu <felipe.bordeu@safrangroup.com>
2023-02-03 17:45:32 +0100

    (ElementNameGenerator.py) Move generated File

--- cpp_generators/ElementNameGenerator.py
+++ cpp_generators/ElementNameGenerator.py
@@ -20,7 +20,7 @@ def GetGeneratedFiles(prefix: str = "cpp_src") -> Tuple[str]:
     Tuple[str]
         list of generated files for this generator
     """
-    return ( os.path.join(prefix ,"FE", "GeneratedElementNames.cpp"), )
+    return ( os.path.join(prefix ,"Containers", "GeneratedElementNames.cpp"), )
 
 def Generate(prefix:str = "cpp_src") :
     """ Run the generation of cpp file using the prefix.
@@ -56,23 +56,6 @@ GeoSupport GeoTet("tet"  ,3);
 GeoSupport GeoPyr("pyr"  ,3);
 GeoSupport GeoWed("wed"  ,3);
 GeoSupport GeoHex("hex"  ,3 );
-
-const std::string Point_1 = "point1";
-const std::string Bar_2 = "bar2";
-const std::string Bar_3 = "bar3";
-const std::string Triangle_6 = "tri6";
-const std::string Quadrangle_8 = "quad8";
-const std::string Quadrangle_9 = "quad9";
-const std::string Tetrahedron_4 = "tet4";
-const std::string Pyramid_5 = "pyr5";
-const std::string Wedge_6 = "wed6";
-const std::string Hexaedron_8 = "hex8";
-const std::string Tetrahedron_10 = "tet10";
-const std::string Pyramid_13 = "pyr13";
-const std::string Wedge_15 = "wed15";
-const std::string Wedge_18 = "wed18";
-const std::string Hexaedron_20 = "hex20";
-const std::string Hexaedron_27 = "hex27";
 
 std::map<std::string,ElementInfo> InitElementNames() {
     std::map<std::string,ElementInfo> ElementNames;
