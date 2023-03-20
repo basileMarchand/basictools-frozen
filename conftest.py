c95de082d2ef888780e4e31c1e4e3d90379adab0
BORDEU Felipe <felipe.bordeu@safrangroup.com>
2023-03-20 16:30:39 +0100

    (conftest.py) Update pytest config

--- conftest.py
+++ conftest.py
@@ -7,26 +7,18 @@
 
 import pytest
 
-class fail_if_not_ok:
-    def __init__(self, f):
-        self.f = f
-
-    def __call__(self):
-        res = self.f()
-
-        assert (res.lower() in ["ok", "skip"] )
+@pytest.hookimpl(tryfirst=True)
+def pytest_pyfunc_call(pyfuncitem):
+    testfunction = pyfuncitem.obj
+    res = testfunction()
+    assert (res.lower() in ["ok", "skip"] )
+    return True
 
 def pytest_pycollect_makeitem(collector, name, obj):
     if name == "CheckIntegrity" and hasattr(obj, "__call__"):
         # Collect CheckIntegrity functions
-        # Decorate them to detect failing return values
-        if hasattr(pytest.Function, "from_parent"):
-            item =  pytest.Function.from_parent(collector, name=name)
-            item.callobj = fail_if_not_ok(obj)
-            return item
-        else:
-            # DEPRECATED pytest API
-            return pytest.Function(name, parent=collector, callobj=fail_if_not_ok(obj))
+        #if hasattr(pytest.Function, "from_parent"):
+        return   pytest.Function.from_parent(collector, name=name)
     else:
         # Fallback to default handler
         return None
