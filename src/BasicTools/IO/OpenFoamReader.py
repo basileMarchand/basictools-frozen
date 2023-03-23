bb030bb4f760f298e8e410b4937618776d1dc221
Fabien Casenave <fabien.casenave@safrangroup.com>
2023-03-23 17:58:58 +0100

    (OpenFoamReader) make CheckIntegrity windows compatible

--- src/BasicTools/IO/OpenFoamReader.py
+++ src/BasicTools/IO/OpenFoamReader.py
@@ -121,7 +121,7 @@ boundaryField
     from BasicTools.Helpers.Tests import TestTempDir
     tempdir = TestTempDir.GetTempPath()
 
-    os.system("mkdir -p "+tempdir+"0.1")
+    os.makedirs(tempdir+"0.1")
 
     f = open(tempdir+"0.1"+os.sep+"U", "w")
     f.write(string)
