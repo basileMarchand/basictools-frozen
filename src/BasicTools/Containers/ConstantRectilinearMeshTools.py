b2a953ab40ef4396c6fad5585ac8c14c4900f36a
BORDEU Felipe <felipe.bordeu@safrangroup.com>
2023-05-06 23:53:32 +0200

    (ConstantRectilinearMeshTools.py) DocStrings

--- src/BasicTools/Containers/ConstantRectilinearMeshTools.py
+++ src/BasicTools/Containers/ConstantRectilinearMeshTools.py
@@ -8,10 +8,25 @@ import numpy as np
 from scipy.sparse import coo_matrix
 
 from BasicTools.Containers.ConstantRectilinearMesh import ConstantRectilinearMesh
-from BasicTools.NumpyDefs import PBasicIndexType
+from BasicTools.NumpyDefs import PBasicIndexType, PBasicFloatType, ArrayLike
 
-def GetSubSuperMesh(inputmesh, _newDimensions):
-    newDimensions = np.array(_newDimensions,dtype=PBasicIndexType)
+def GetSubSuperMesh(inputmesh:ConstantRectilinearMesh, newDimensions:ArrayLike)-> ConstantRectilinearMesh:
+    """Generate a new ConstantRectilinearMesh with the same volume (origin and size )
+    but with different number of elements
+
+    Parameters
+    ----------
+    inputmesh : ConstantRectilinearMesh
+        the input mesh to recover the origin and size
+    newDimensions : ArrayLike
+        the dimensions of the new mesh
+
+    Returns
+    -------
+    ConstantRectilinearMesh
+        the new mesh
+    """
+    newDimensions = np.array(newDimensions, dtype=PBasicIndexType)
     ## to generate meshes with more or less elements in each directions
     ## return the mesh
     newSpac = ((inputmesh.GetDimensions()-1)*inputmesh.GetSpacing()).astype(float)/(newDimensions-1)
@@ -23,58 +38,89 @@ def GetSubSuperMesh(inputmesh, _newDimensions):
 
     return res
 
-def GetNodeTrasfertMatrix(inputmesh,destinationmesh):
+def GetNodeTransferMatrix(inputMesh:ConstantRectilinearMesh, destinationMesh:ConstantRectilinearMesh)-> coo_matrix:
+    """return the transfer operator to transfer a field defined on the nodes of the input mesh (inputMesh)
+    to the output mesh. Data is extrapolated outsize if a point of the destination mesh lies outside the input mesh
+
+    Parameters
+    ----------
+    inputMesh : ConstantRectilinearMesh
+        the mesh defining the support of the point field to be transferred
+    destinationMesh : ConstantRectilinearMesh
+        destination mesh
+
+    Returns
+    -------
+    np.ndarray
+        OP: The transfer operation (sparse matrix in coo)
+        field in the new mesh = OP.dot(field defined in the input Mesh)
+    """
     # newVector   = oldToNew * oldVector
-    nbNodes = 2**inputmesh.GetDimensionality()
-    oldToNewVals = np.zeros((destinationmesh.GetNumberOfNodes(),nbNodes))
-    oldToNewIK = np.zeros((destinationmesh.GetNumberOfNodes(),nbNodes), dtype=np.int_)
-    oldToNewJK = np.zeros((destinationmesh.GetNumberOfNodes(),nbNodes), dtype=np.int_)
+    nbNodes = 2**inputMesh.GetDimensionality()
+    oldToNewVals = np.zeros((destinationMesh.GetNumberOfNodes(),nbNodes))
+    oldToNewIK = np.zeros((destinationMesh.GetNumberOfNodes(),nbNodes), dtype=np.int_)
+    oldToNewJK = np.zeros((destinationMesh.GetNumberOfNodes(),nbNodes), dtype=np.int_)
 
-    for i in range(destinationmesh.GetNumberOfNodes()):
+    for i in range(destinationMesh.GetNumberOfNodes()):
 
-        pos= destinationmesh.GetPosOfNode(i)
-        el = inputmesh.GetElementAtPos(pos)
-        coon = inputmesh.GetConnectivityForElement(el)
-        xiChiEta = inputmesh.GetElementShapeFunctionsAtPos(el,pos)
+        pos= destinationMesh.GetPosOfNode(i)
+        el = inputMesh.GetElementAtPos(pos)
+        coon = inputMesh.GetConnectivityForElement(el)
+        xiChiEta = inputMesh.GetElementShapeFunctionsAtPos(el,pos)
         oldToNewVals[i,:] = xiChiEta
         oldToNewIK[i,:] = i
         oldToNewJK[i,:] = coon
 
-    oldToNew =  coo_matrix((oldToNewVals.ravel(), (oldToNewIK.flatten(), oldToNewJK.flatten())), shape=(destinationmesh.GetNumberOfNodes(), inputmesh.GetNumberOfNodes())).tocsc()
+    oldToNew =  coo_matrix((oldToNewVals.ravel(), (oldToNewIK.flatten(), oldToNewJK.flatten())), shape=(destinationMesh.GetNumberOfNodes(), inputMesh.GetNumberOfNodes())).tocsc()
 
     return oldToNew
 
-def GetElementTrasfertMatrix(inputmesh, destinationmesh):
+def GetElementTransferMatrix(inputMesh:ConstantRectilinearMesh, destinationMesh:ConstantRectilinearMesh)-> coo_matrix:
+    """return the transfer operator to transfer a field defined on the elements of the input mesh (inputMesh)
+    to the output mesh.
 
-    if not isinstance(inputmesh, ConstantRectilinearMesh):
+    Parameters
+    ----------
+    inputMesh : ConstantRectilinearMesh
+        the mesh defining the support of the element field to be transferred
+    destinationMesh : ConstantRectilinearMesh
+        destination mesh
+
+    Returns
+    -------
+    np.ndarray
+        OP: The transfer operation (sparse matrix in coo)
+        field in the new mesh = OP.dot(field defined in the input Mesh)
+    """
+    if not isinstance(inputMesh, ConstantRectilinearMesh):
         raise Exception("First argument must be a ConstantRectilinearMesh")
     nps = 3
-    nps3 = nps**inputmesh.GetDimensionality()
-    oldToNewVals = np.zeros((destinationmesh.GetNumberOfNodes(),nps3))
-    oldToNewIK = np.zeros((destinationmesh.GetNumberOfNodes(),nps3), dtype=np.int_)
-    oldToNewJK = np.zeros((destinationmesh.GetNumberOfNodes(),nps3), dtype=np.int_)
+    nps3 = nps**inputMesh.GetDimensionality()
+    oldToNewVals = np.zeros((destinationMesh.GetNumberOfNodes(),nps3))
+    oldToNewIK = np.zeros((destinationMesh.GetNumberOfNodes(),nps3), dtype=np.int_)
+    oldToNewJK = np.zeros((destinationMesh.GetNumberOfNodes(),nps3), dtype=np.int_)
 
-    numberOfElementsDest = destinationmesh.GetNumberOfElements(dim = destinationmesh.GetElementsDimensionality() )
-    numberOfElementsInp = inputmesh.GetNumberOfElements(dim = inputmesh.GetElementsDimensionality() )
+    numberOfElementsDest = destinationMesh.GetNumberOfElements(dim = destinationMesh.GetElementsDimensionality() )
+    numberOfElementsInp = inputMesh.GetNumberOfElements(dim = inputMesh.GetElementsDimensionality() )
 
     for i in  range(numberOfElementsDest):
-        coon = destinationmesh.GetConnectivityForElement(i)
+        coon = destinationMesh.GetConnectivityForElement(i)
 
-        n0pos = destinationmesh.GetPosOfNode(coon[0])
+        n0pos = destinationMesh.GetPosOfNode(coon[0])
         cpt =0
         for cx in range(0,nps):
             for cy in range(0,nps):
-                if inputmesh.GetDimensionality() == 3:
+                if inputMesh.GetDimensionality() == 3:
                     for cz in range(0,nps):
-                        pos = n0pos + destinationmesh.GetSpacing()*([cx+0.5,cy+0.5,cz+0.5])/nps
-                        el = inputmesh.GetElementAtPos(pos)
+                        pos = n0pos + destinationMesh.GetSpacing()*([cx+0.5,cy+0.5,cz+0.5])/nps
+                        el = inputMesh.GetElementAtPos(pos)
                         oldToNewVals[i,cpt] += 1./nps3
                         oldToNewIK[i,cpt] += i
                         oldToNewJK[i,cpt] += el
                         cpt +=1
                 else:
-                    pos = n0pos + destinationmesh.GetSpacing()*([cx+0.5,cy+0.5])/nps
-                    el = inputmesh.GetElementAtPos(pos)
+                    pos = n0pos + destinationMesh.GetSpacing()*([cx+0.5,cy+0.5])/nps
+                    el = inputMesh.GetElementAtPos(pos)
                     oldToNewVals[i,cpt] += 1./nps3
                     oldToNewIK[i,cpt] += i
                     oldToNewJK[i,cpt] += el
@@ -86,17 +132,37 @@ def GetElementTrasfertMatrix(inputmesh, destinationmesh):
 
 
 #------------------------------------------------------------------------------
-def CreateSquare(dimensions=[2,2], origin=[-1.0,-1.0], spacing=[1.,1.]):
-    spacing = np.array(spacing,dtype=float)
-    origin = np.array(origin,dtype=float)
+def CreateSquare(dimensions:ArrayLike=[2,2], origin:ArrayLike=[-1.0,-1.0], spacing:ArrayLike=[1.,1.]) ->ConstantRectilinearMesh:
+    """Create ConstantRectilinearMesh using the dimension, origin and spacing.
+    Extra data is added:
+        the bulk element with tag "2D"
+        the border elements with tags "X0", "X1", "Y0", "Y1",
+        the nodes of the corner with tags "x0y0", "x1y0", "x1y1", "x0y1"
+
+    Parameters
+    ----------
+    dimensions : ArrayLike, optional
+        Number of node in each dimension, by default [2,2]
+    origin : ArrayLike, optional
+        position of the first corner, by default [-1.0,-1.0]
+    spacing : ArrayLike, optional
+        size of the elements, by default [1.,1.]
+
+    Returns
+    -------
+    ConstantRectilinearMesh
+        the
+    """
+    spacing = np.asarray(spacing,dtype=PBasicFloatType)
+    origin = np.asarray(origin,dtype=PBasicFloatType)
     from BasicTools.Containers.ConstantRectilinearMesh import ConstantRectilinearMesh
     from BasicTools.Containers.UnstructuredMeshModificationTools import ComputeSkin
     import BasicTools.Containers.ElementNames as EN
 
     myMesh = ConstantRectilinearMesh(dim=2)
-    myMesh.SetDimensions(dimensions);
-    myMesh.SetOrigin(origin);
-    myMesh.SetSpacing(spacing);
+    myMesh.SetDimensions(dimensions)
+    myMesh.SetOrigin(origin)
+    myMesh.SetSpacing(spacing)
 
     # coorners
     d = np.array(dimensions)-1
@@ -138,33 +204,68 @@ def CreateSquare(dimensions=[2,2], origin=[-1.0,-1.0], spacing=[1.,1.]):
     return myMesh
 
 
-def CreateMesh(dim):
+def CreateMesh(dim:int):
+    """Helper class to create a ConstantRectilinearMesh of dimension dim and with only one element of size 1"""
+
     myMesh = ConstantRectilinearMesh(dim)
-    myMesh.SetDimensions([2,]*dim);
-    myMesh.SetSpacing([1, ]*dim);
-
+    myMesh.SetDimensions([2,]*dim)
+    myMesh.SetSpacing([1, ]*dim)
     return myMesh
-def CheckIntegrity_GetSubSuperMesh(dim):
 
+def CheckIntegrity_GetSubSuperMesh(dim):
+    """CheckIntegrity function
+
+    Parameters
+    ----------
+    dim : int
+        dimensionality of the mesh to test
+    """
     newmesh = GetSubSuperMesh(CreateMesh(dim),[3,]*dim)
 
-def CheckIntegrity_GetNodeTrasfertMatrix(dim):
+def CheckIntegrity_GetNodeTransferMatrix(dim:int):
+    """CheckIntegrity function
+
+    Parameters
+    ----------
+    dim : int
+        dimensionality of the mesh to test
+    """
+
     mesh1 = CreateMesh(dim)
     mesh2 = GetSubSuperMesh(mesh1,[3,]*dim)
 
-    TMatrix = GetNodeTrasfertMatrix(mesh1,mesh2)
+    TMatrix = GetNodeTransferMatrix(mesh1,mesh2)
 
-def CheckIntegrity_GetElementTrasfertMatrix(dim):
+def CheckIntegrity_GetElementTransferMatrix(dim:int):
+    """CheckIntegrity function
+
+    Parameters
+    ----------
+    dim : int
+        dimensionality of the mesh to test
+    """
     mesh1 = CreateMesh(dim)
     mesh2 = GetSubSuperMesh(mesh1,[3,]*dim)
 
-    TMatrix = GetElementTrasfertMatrix(mesh1,mesh2)
+    TMatrix = GetElementTransferMatrix(mesh1,mesh2)
 
-def CheckIntegrity(GUI=False):
+def CheckIntegrity(GUI=False)-> str:
+    """CheckIntegrity function. Tests
+
+    Parameters
+    ----------
+    GUI : bool, optional
+        if True, generate (in some case) an output on a new window, by default False
+
+    Returns
+    -------
+    str
+        ok if all ok
+    """
     for dim in [2,3]:
         CheckIntegrity_GetSubSuperMesh(dim)
-        CheckIntegrity_GetNodeTrasfertMatrix(dim)
-        CheckIntegrity_GetElementTrasfertMatrix(dim)
+        CheckIntegrity_GetNodeTransferMatrix(dim)
+        CheckIntegrity_GetElementTransferMatrix(dim)
     CreateSquare()
     return  "ok"
 
