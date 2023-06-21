7356e713bc47b4e84e6294fdd39d7ace030d6adf
Felipe Bordeu <felipe.bordeu@safrangroup.com>
2023-06-21 16:11:15 +0200

    (CGNSBridge.py) Remove need of CGNS module to read and write files

--- src/BasicTools/Bridges/CGNSBridge.py
+++ src/BasicTools/Bridges/CGNSBridge.py
@@ -5,46 +5,46 @@
 #
 import numpy as np
 
-from CGNS.MAP import load, save
-import CGNS.PAT.cgnskeywords as CGK
-import CGNS.PAT.cgnsutils as CGU
-import CGNS.PAT.cgnslib as CGL
+#from CGNS.MAP import load, save
+#import CGNS.PAT.cgnsutils as CGU
+#import CGNS.PAT.cgnslib as CGL
 
 from BasicTools.NumpyDefs import PBasicFloatType, PBasicIndexType
 from BasicTools.Containers.UnstructuredMesh import UnstructuredMesh, ElementsContainer
 import BasicTools.Containers.ElementNames as EN
 
 CGNSNameToBasicTools = {}
-CGNSNameToBasicTools[CGK.NODE_s] = EN.Point_1
+CGNSNameToBasicTools["NODE"] = EN.Point_1
 # 1D
-CGNSNameToBasicTools[CGK.BAR_2_s] = EN.Bar_2
-CGNSNameToBasicTools[CGK.BAR_3_s] = EN.Bar_3
+CGNSNameToBasicTools["BAR_2"] = EN.Bar_2
+CGNSNameToBasicTools["BAR_3"] = EN.Bar_3
 # 2D
-CGNSNameToBasicTools[CGK.TRI_3_s] = EN.Triangle_3
-CGNSNameToBasicTools[CGK.TRI_6_s] = EN.Triangle_6
-CGNSNameToBasicTools[CGK.QUAD_4_s] = EN.Quadrangle_4
-CGNSNameToBasicTools[CGK.QUAD_8_s] = EN.Quadrangle_8
-CGNSNameToBasicTools[CGK.QUAD_9_s] = EN.Quadrangle_9
+CGNSNameToBasicTools["TRI_3"] = EN.Triangle_3
+CGNSNameToBasicTools["TRI_6"] = EN.Triangle_6
+CGNSNameToBasicTools["QUAD_4"] = EN.Quadrangle_4
+CGNSNameToBasicTools["QUAD_8"] = EN.Quadrangle_8
+CGNSNameToBasicTools["QUAD_9"] = EN.Quadrangle_9
 # 3D
-CGNSNameToBasicTools[CGK.TETRA_4_s] = EN.Tetrahedron_4
-CGNSNameToBasicTools[CGK.TETRA_10_s] = EN.Tetrahedron_10
-CGNSNameToBasicTools[CGK.PYRA_5_s] = EN.Pyramid_5
-CGNSNameToBasicTools[CGK.PYRA_13_s] = EN.Pyramid_13
-CGNSNameToBasicTools[CGK.PENTA_6_s] = EN.Wedge_6
-CGNSNameToBasicTools[CGK.PENTA_15_s] = EN.Wedge_15
-CGNSNameToBasicTools[CGK.PENTA_18_s] = EN.Wedge_18
-CGNSNameToBasicTools[CGK.HEXA_8_s] = EN.Hexaedron_8
-CGNSNameToBasicTools[CGK.HEXA_20_s] = EN.Hexaedron_20
-CGNSNameToBasicTools[CGK.HEXA_27_s] = EN.Hexaedron_27
+CGNSNameToBasicTools["TETRA_4"] = EN.Tetrahedron_4
+CGNSNameToBasicTools["TETRA_10"] = EN.Tetrahedron_10
+CGNSNameToBasicTools["PYRA_5"] = EN.Pyramid_5
+CGNSNameToBasicTools["PYRA_13"] = EN.Pyramid_13
+CGNSNameToBasicTools["PENTA_6"] = EN.Wedge_6
+CGNSNameToBasicTools["PENTA_15"] = EN.Wedge_15
+CGNSNameToBasicTools["PENTA_18"] = EN.Wedge_18
+CGNSNameToBasicTools["HEXA_8"] = EN.Hexaedron_8
+CGNSNameToBasicTools["HEXA_20"] = EN.Hexaedron_20
+CGNSNameToBasicTools["HEXA_27"] = EN.Hexaedron_27
 # when adding a new element please add the Pertmutaion
 #https://cgns.github.io/CGNS_docs_current/sids/conv.html#unstructgrid
-#example  CGNSToBasicToolsPermutation[CGK.TETRA_4_s] = [0 3 2 1]
+#example  CGNSToBasicToolsPermutation["TETRA_4"] = [0 3 2 1]
 CGNSToBasicToolsPermutation = {}
-CGNSToBasicToolsPermutation[CGK.HEXA_20_s] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 16, 17 ,18, 19, 12, 13, 14, 15]
-CGNSToBasicToolsPermutation[CGK.HEXA_27_s] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 16, 17 ,18, 19, 12, 13, 14, 15, 24, 22, 21, 23, 20, 25, 26]
+CGNSToBasicToolsPermutation["HEXA_20"] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 16, 17 ,18, 19, 12, 13, 14, 15]
+CGNSToBasicToolsPermutation["HEXA_27"] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 16, 17 ,18, 19, 12, 13, 14, 15, 24, 22, 21, 23, 20, 25, 26]
 
+CGNS_ElementType_l = ['Null', 'UserDefined', 'NODE', 'BAR_2', 'BAR_3', 'TRI_3', 'TRI_6', 'QUAD_4', 'QUAD_8', 'QUAD_9', 'TETRA_4', 'TETRA_10', 'PYRA_5', 'PYRA_14', 'PENTA_6', 'PENTA_15', 'PENTA_18', 'HEXA_8', 'HEXA_20', 'HEXA_27', 'MIXED', 'PYRA_13', 'NGON_n', 'NFACE_n', 'BAR_4', 'TRI_9', 'TRI_10', 'QUAD_12', 'QUAD_16', 'TETRA_16', 'TETRA_20', 'PYRA_21', 'PYRA_29', 'PYRA_30', 'PENTA_24', 'PENTA_38', 'PENTA_40', 'HEXA_32', 'HEXA_56', 'HEXA_64', 'BAR_5', 'TRI_12', 'TRI_15', 'QUAD_P4_16', 'QUAD_25', 'TETRA_22', 'TETRA_34', 'TETRA_35', 'PYRA_P4_29', 'PYRA_50', 'PYRA_55', 'PENTA_33', 'PENTA_66', 'PENTA_75', 'HEXA_44', 'HEXA_98', 'HEXA_125']
 
-CGNSNumberToBasicTools = { CGK.ElementType_l.index(k):v for k,v in CGNSNameToBasicTools.items() }
+CGNSNumberToBasicTools = { CGNS_ElementType_l.index(k):v for k,v in CGNSNameToBasicTools.items() }
 
 BasicToolsToCGNSNames = { y:x for x,y in CGNSNameToBasicTools.items() }
 BasicToolsToCGNSNumber = { y:x for x,y in CGNSNumberToBasicTools.items() }
@@ -53,7 +53,7 @@ BasicToolsToCGNSNumber = { y:x for x,y in CGNSNumberToBasicTools.items() }
 def GetCGNSNumberToBasicTools(cgnsNumber):
     res = CGNSNumberToBasicTools.get(cgnsNumber,None)
     if res is None:
-        raise Exception(f"Elements of type : {CGK.ElementType_l[cgnsNumber]} ({cgnsNumber}) not coded yet")
+        raise Exception(f"CGNC elements of number : {cgnsNumber} not coded yet")
     return res
 
 
@@ -63,66 +63,100 @@ def __ReadIndex(pyTree):
     return np.hstack( (a,b) )
 
 def __ReadIndexArray(pyTree):
-    indexArrayPaths = CGU.getAllNodesByTypeSet(pyTree, ['IndexArray_t'])
+    indexArrayPaths = GetAllNodesByTypeSet(pyTree, 'IndexArray_t')
     res =[]
     for indexArrayPath in indexArrayPaths:
-        data = CGU.getNodeByPath(pyTree, indexArrayPath)
+        data = GetNodeByPath(pyTree, indexArrayPath)[0]
         res.extend(data[1].ravel())
     return np.array(res, dtype=PBasicIndexType).ravel()
 
 def __ReadIndexRange(pyTree):
-    indexRangePaths = CGU.getAllNodesByTypeSet(pyTree, ['IndexRange_t'])
+    indexRangePaths = GetAllNodesByTypeSet(pyTree, 'IndexRange_t')
     res =[]
     #if len(indexRangePaths) == 0:
     #    return np.zeros((0), dtype = PBasicIndexType)
 
     for indexRangePath in indexRangePaths:
-        indexRange = CGU.getNodeByPath(pyTree, indexRangePath)[1]
+        indexRange = GetNodeByPath(pyTree, indexRangePath)[0][1]
         begin = indexRange[0]#[:,0]
         end = indexRange[1]#[:,1]
         res.extend(np.arange(begin, end+1).ravel())
 
     return np.array(res, dtype = PBasicIndexType).ravel()
 
+def GetRecursiveObjects(pyTree, filter, state= None):
+    res = list()
+    state, lres = filter(state, pyTree)
+    res.extend(lres)
+    for subTree in pyTree[2]:
+        lstate, lres = GetRecursiveObjects(subTree,filter,state)
+        res.extend(lres)
+    return state, res
+
+
+def GetAllNodesByTypeSet(pyTree, typeset):
+
+    def fil(state, data):
+        if state is None:
+            state = ""
+        state += "/" + data[0]
+        if data[3] == typeset:
+            return state, [state]
+        else:
+            return state, []
+
+    return GetRecursiveObjects(pyTree, fil)[1]
+
+def GetNodeByPath(pyTree, path):
+
+    def fil(state, data):
+        if len(state)==1 and data[0] == state[0]:
+            return state[1:], [data]
+        else:
+            return state[1:], []
+
+    return GetRecursiveObjects(pyTree, fil, path.strip("/").split("/"))[1]
+
 def CGNSToMesh(pyTree, baseNumberOrName= 0, zoneNumberOrName = 0)-> UnstructuredMesh:
 
     res = UnstructuredMesh()
 
     if type(baseNumberOrName) is int:
-        basepath = CGU.getAllNodesByTypeSet(pyTree,[CGK.CGNSBase_ts])[baseNumberOrName][1:]
+        basepath = GetAllNodesByTypeSet(pyTree,"CGNSBase_t")[baseNumberOrName]
     else:
         basepath = baseNumberOrName
 
-    base = CGU.getNodeByPath(pyTree,basepath)
+    base = GetNodeByPath(pyTree,basepath)[0]
 
     if type(zoneNumberOrName) is int:
-        zonepath = CGU.getAllNodesByTypeSet(base,[CGK.Zone_t])[zoneNumberOrName]
+        zonepath = GetAllNodesByTypeSet(base,"Zone_t")[zoneNumberOrName]
     else:
         zonepath = zoneNumberOrName
 
-    zonePyTree = CGU.getNodeByPath(pyTree,"/"+ zonepath)
+    zonePyTree = GetNodeByPath(base,"/"+ zonepath)[0]
 
-    gridCoordinatesPath = CGU.getAllNodesByTypeSet(zonePyTree, [CGK.GridCoordinates_ts])[0]
 
-    gx = CGU.getNodeByPath(zonePyTree,gridCoordinatesPath+'/CoordinateX')[1]
+    gridCoordinatesPath = GetAllNodesByTypeSet(zonePyTree, "GridCoordinates_t")[0]
+
+    gx = GetNodeByPath(zonePyTree,gridCoordinatesPath+'/CoordinateX')[0][1]
     res.nodes = np.empty((gx.shape[0],3), dtype= PBasicFloatType)
     res.nodes[:,0] = gx
-    res.nodes[:,1] = CGU.getNodeByPath(zonePyTree,gridCoordinatesPath+'/CoordinateY')[1]
-    res.nodes[:,2] = CGU.getNodeByPath(zonePyTree,gridCoordinatesPath+'/CoordinateZ')[1]
+    res.nodes[:,1] = GetNodeByPath(zonePyTree,gridCoordinatesPath+'/CoordinateY')[0][1]
+    res.nodes[:,2] = GetNodeByPath(zonePyTree,gridCoordinatesPath+'/CoordinateZ')[0][1]
     res.originalIDNodes = np.arange(1, res.nodes.shape[0]+1, dtype= PBasicIndexType)
 
     #elements
-    elementsPaths = CGU.getAllNodesByTypeSet(zonePyTree, [CGK.Elements_ts])
+    elementsPaths = GetAllNodesByTypeSet(zonePyTree, "Elements_t")
 
     for elementsPath in elementsPaths:
-        cgnsElements = CGU.getNodeByPath(zonePyTree,elementsPath)
+        cgnsElements = GetNodeByPath(zonePyTree,elementsPath)[0]
         cgnsUserName = "Elements_" + cgnsElements[0]
         cgnsElemType = cgnsElements[1][0]
         originalIds = __ReadIndexRange(cgnsElements)
 
         if cgnsElemType == 20: # we are in mixed mode:
             cpt = 0
-            elementConnectivity = CGU.getNodeByPath(cgnsElements,'/ElementConnectivity')[1]
+            elementConnectivity = GetNodeByPath(cgnsElements,cgnsUserName+'/ElementConnectivity')[0][1]
             for oid in originalIds:
                 basicToolsElemType = GetCGNSNumberToBasicTools(elementConnectivity[cpt])
                 cpt +=1
@@ -136,7 +170,7 @@ def CGNSToMesh(pyTree, baseNumberOrName= 0, zoneNumberOrName = 0)-> Unstructured
                 elems.tags.CreateTag(cgnsUserName,False).AddToTag(elcpt-1)
         else:
             basicToolsElemType = GetCGNSNumberToBasicTools(cgnsElemType)
-            elementConnectivity = np.asarray(CGU.getNodeByPath(cgnsElements,'/ElementConnectivity')[1], dtype=PBasicIndexType).reshape( (-1, EN.numberOfNodes[basicToolsElemType]  ) )-1
+            elementConnectivity = np.asarray(GetNodeByPath(cgnsElements,cgnsUserName+'/ElementConnectivity')[0][1], dtype=PBasicIndexType).reshape( (-1, EN.numberOfNodes[basicToolsElemType]  ) )-1
             elems: ElementsContainer = res.elements.GetElementsOfType(basicToolsElemType)
 
             if CGNSToBasicToolsPermutation.get(cgnsElemType,None) is not None:
@@ -147,21 +181,21 @@ def CGNSToMesh(pyTree, baseNumberOrName= 0, zoneNumberOrName = 0)-> Unstructured
 
             elems.tags.CreateTag(cgnsUserName,False).AddToTag(np.arange(elcpt,nelcpt))
 
-    datasPaths = CGU.getAllNodesByTypeSet(zonePyTree, [CGK.FlowSolution_ts])
+    datasPaths = GetAllNodesByTypeSet(zonePyTree, "FlowSolution_t")
     for dataPath in datasPaths :
-        datas = CGU.getNodeByPath(zonePyTree,dataPath)
-        gl = CGU.getAllNodesByTypeSet(datas, [CGK.GridLocation_ts])
+        datas = GetNodeByPath(zonePyTree,dataPath)[0]
+        gl = GetAllNodesByTypeSet(datas, "GridLocation_t")
 
         store  = res.nodeFields
         if len(gl) > 0:
-            if "".join(np.array(CGU.getNodeByPath(datas,gl[0])[1], dtype=str) )== CGK.CellCenter_s:
+            if "".join(np.array(GetNodeByPath(datas,gl[0])[0][1], dtype=str) )== "CellCenter":
                 store  = res.elemFields
-            if "".join(np.array(CGU.getNodeByPath(datas,gl[0])[1], dtype=str) )== CGK.Vertex_s:
+            if "".join(np.array(GetNodeByPath(datas,gl[0])[0][1], dtype=str) )== "Vertex":
                 store  = res.nodeFields
 
-        fieldPaths = CGU.getAllNodesByTypeSet(datas, [CGK.DataArray_ts])
+        fieldPaths = GetAllNodesByTypeSet(datas, ["DataArray_t"])
         for fieldPath in fieldPaths:
-            fieldData = CGU.getNodeByPath(datas,fieldPath)
+            fieldData = GetNodeByPath(datas,fieldPath)[0]
             dataName = fieldData[0]
             data = fieldData[1]
             if  dataName == "OriginalIds" and store is res.nodeFields:
@@ -171,13 +205,13 @@ def CGNSToMesh(pyTree, baseNumberOrName= 0, zoneNumberOrName = 0)-> Unstructured
             else:
                 res.nodeFields[dataName] = np.asarray(data)
 
-    ZoneBCPaths = CGU.getAllNodesByTypeSet(zonePyTree, [CGK.ZoneBC_ts])
+    ZoneBCPaths = GetAllNodesByTypeSet(zonePyTree, ["ZoneBC_t"])
     for ZoneBCPath in ZoneBCPaths:
-        ZoneBC = CGU.getNodeByPath(zonePyTree,ZoneBCPath)
+        ZoneBC = GetNodeByPath(zonePyTree,ZoneBCPath)[0]
 
-        BCPaths = CGU.getAllNodesByTypeSet(ZoneBC, [CGK.BC_ts])
+        BCPaths = GetAllNodesByTypeSet(ZoneBC, ["BC_t"])
         for BCPath in BCPaths:
-            BCNode = CGU.getNodeByPath(ZoneBC,BCPath)
+            BCNode = GetNodeByPath(ZoneBC,BCPath)[0]
             BCName = str(BCNode[0])
 
 
@@ -190,6 +224,11 @@ def CGNSToMesh(pyTree, baseNumberOrName= 0, zoneNumberOrName = 0)-> Unstructured
                 res.AddElementToTagUsingOriginalId(indices, BCName)
 
     res.PrepareForOutput()
+    return res
+
+def NewDataArray(father, name, data):
+    res =  [name, data, [], 'DataArray_t']
+    father[2].append(res)
     return res
 
 def MeshToCGNS( mesh: UnstructuredMesh, outputPyTree = None,  baseNumberOrName= 0, zoneNumberOrName = 0) :
@@ -205,27 +244,32 @@ def MeshToCGNS( mesh: UnstructuredMesh, outputPyTree = None,  baseNumberOrName= 
         zoneName = f"{baseNumberOrName}"
 
     if outputPyTree is None:
-        outputPyTree=CGL.newCGNSTree()
+        outputPyTree=['CGNSTree', None, [['CGNSLibraryVersion', np.array([4.], dtype=np.float32), [], 'CGNSLibraryVersion_t']], 'CGNSTree_t']
         physicalDim = mesh.GetPointsDimensionality()
         topologicalDim = mesh.GetElementsDimensionality()
-        base = CGL.newBase(outputPyTree,baseName,physicalDim,topologicalDim) # name, physical dim, topological dim
+        base = [baseName, np.array([physicalDim, topologicalDim], dtype=np.int32), [], 'CGNSBase_t']  # name, physical dim, topological dim
+        outputPyTree[2].append(base)
 
         s = np.array([mesh.GetNumberOfNodes(),mesh.GetNumberOfElements(),0],dtype=np.int32)
-        grid = CGL.newZone(base,zoneName,s,CGK.Unstructured_s)
+
+
+        grid = [zoneName, s, [['ZoneType', np.array([b'U', b'n', b's', b't', b'r', b'u', b'c', b't', b'u', b'r', b'e',b'd'], dtype='|S1'), [], 'ZoneType_t']], 'Zone_t']
+        base[2].append(grid)
     else:
-        grid = CGU.getNodeByPath(outputPyTree,f"/{baseName}/{zoneName}/")[1]
+        grid = GetNodeByPath(outputPyTree,f"/{baseName}/{zoneName}/")[0][1]
 
     #add nodes
+    import CGNS.PAT.cgnslib as CGL
     gridCoordinates = CGL.newGridCoordinates(grid,"GridCoordinates")
     for i,coord in enumerate(["X", "Y", "Z"]):
-        da = CGL.newDataArray(gridCoordinates, "Coordinate"+coord, np.copy(mesh.nodes[:,i]) )
+        da = NewDataArray(gridCoordinates, "Coordinate"+coord, np.copy(mesh.nodes[:,i]) )
 
 
     #nodes tags
     if len(mesh.nodesTags):
         zbg = CGL.newZoneBC(grid)
         for tag in mesh.nodesTags:
-           tt = CGL.newBC(zbg,tag.name,pttype=CGK.IndexArray_ts)
+           tt = CGL.newBC(zbg,tag.name,pttype="IndexArray_t")
            tt[2][0][0] = "PointList"
            tt[2][0][1] = tag.GetIds()+1
 
@@ -237,16 +281,16 @@ def MeshToCGNS( mesh: UnstructuredMesh, outputPyTree = None,  baseNumberOrName= 
         CGL.newElements(grid,"Elements_"+ BasicToolsToCGNSNames[name],BasicToolsToCGNSNumber[name], np.array((data.globaloffset+1, data.globaloffset+nbelem) ), econnectivity= (data.connectivity+1).ravel() )
 
 
-    flowSolution = CGL.newFlowSolution(grid, "PointData", gridlocation=CGK.Vertex_s)
-    da = CGL.newDataArray(flowSolution, "OriginalIds", mesh.originalIDNodes )
+    flowSolution = CGL.newFlowSolution(grid, "PointData", gridlocation="Vertex")
+    da = NewDataArray(flowSolution, "OriginalIds", mesh.originalIDNodes )
     for name, pointData in mesh.nodeFields.items():
-        da = CGL.newDataArray(flowSolution, name,pointData)
+        da = NewDataArray(flowSolution, name,pointData)
 
     ## elem fields
-    flowSolutionCell = CGL.newFlowSolution(grid, "CellData", gridlocation=CGK.CellCenter_s)
-    da = CGL.newDataArray(flowSolutionCell, "OriginalIds", mesh.GetElementsOriginalIDs() )
+    flowSolutionCell = CGL.newFlowSolution(grid, "CellData", gridlocation="CellCenter")
+    da = NewDataArray(flowSolutionCell, "OriginalIds", mesh.GetElementsOriginalIDs() )
     for name, cellData in mesh.elemFields.items():
-        CGL.newDataArray(flowSolutionCell, name, np.copy(cellData))
+        NewDataArray(flowSolutionCell, name, np.copy(cellData))
 
     return outputPyTree
 
@@ -258,6 +302,10 @@ def CheckIntegrity(GUI=False):
 
     import BasicTools.IO.GeofReader as GR
     res = GR.ReadGeof(fileName=BasicToolsTestData.GetTestDataPath() + "UtExample/cube.geof")
+
+    from BasicTools.Containers.UnstructuredMeshCreationTools import CreateCube
+    res = CreateCube()
+
 
     res.VerifyIntegrity()
     resII = CGNSToMesh(MeshToCGNS(res))
