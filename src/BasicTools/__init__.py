8105d640b2a899c36700e9b58e975f9c9ef88bbe
BORDEU Felipe <felipe.bordeu@safrangroup.com>
2023-07-25 16:00:13 +0200

    Add Preload function to preload all BasicTools in memory

--- src/BasicTools/__init__.py
+++ src/BasicTools/__init__.py
@@ -23,11 +23,37 @@ __copyright__ = "{}, {}".format(__copyright_years__,__copyright_holder__)
 __license__ = "BSD 3-Clause License"
 __version__ = "1.9.7"
 
+def Preload(subSubModule= "BasicTools"):
+    import time
+    st = time.time()
+    from BasicTools.Helpers.Tests import __tryImport
+    try:
+        __tryImport(subSubModule,{},stopAtFirstError=True,modulesToTreat=[],modulesToSkip=[])
+        pass
+    except Exception as e:
+        print(e)
+        print('Error Loading module : ' + subSubModule + ' (Current folder'+os.getcwd()+')'  )
+        print('-*-*-*-*-*-*> missing CheckIntegrity()??? <*-*-*-*-*-*--'  )
+        raise
+    #from BasicTools.Helpers.Tests import RunTests
+    #RunTests()
+    if subSubModule == "BasicTools":
+        import BasicTools.Containers.NativeTransfer
+        import BasicTools.Linalg.NativeEigenSolver
+        import BasicTools.FE.Integrators.NativeIntegration
+        import BasicTools.FE.WeakForms.NativeNumericalWeakForm
+        import BasicTools.Containers.NativeUnstructuredMesh
+        import BasicTools.FE.Numberings.NativeDofNumbering
+        import BasicTools.FE.Spaces.NativeSpace
+        import BasicTools.Containers.NativeFilters
+
+    return time.time()-st
 
 def main():
     print(" {} version {}".format(__name__,__version__))
     print(" Copyright (c) {}".format(__copyright__))
     print("")
+
 
     from BasicTools.Helpers.Tests import RunTests
     import sys
