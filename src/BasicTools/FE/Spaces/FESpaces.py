6be7e1f520e16d5ba0b0c0e43c5c8c01f5a4c1c6
BORDEU Felipe <felipe.bordeu@safrangroup.com>
2023-07-28 18:33:48 +0200

    (FESpaces.py) be sure to preload all the available spaces

--- src/BasicTools/FE/Spaces/FESpaces.py
+++ src/BasicTools/FE/Spaces/FESpaces.py
@@ -110,7 +110,7 @@ AllSpaces = AllSpacesType({'LagrangeSpaceGeo':LagrangeSpaceGeo,
 def InitAllSpaces() -> None:
     """Function to create all the spaces, this need to be called at the beginning of the app in a multi threated app
     """
-    for spaces in [ConstantSpaceGlobal, LagrangeSpaceP0, LagrangeSpaceGeo, LagrangeSpaceP1, LagrangeSpaceP2]:
+    for spaces in AllSpaces.values():
         for s in spaces.values():
             s.Create()
 
